6a0871bc6c4851b5311f0f4b9b57eb819e730843
Edward <blingwang@gmail.com>
2013-07-16 17:53:23 -0500

    Create CountInversions.java

---
+++ white-board/CountInversions.java
@@ -0,0 +1,41 @@
+// Given array A[1...n], for every i<j, find all inversion pairs such that A[i]>A[j]
+class CountInversions {
+    public int count(int[] a) {
+        int[] aux = new int[a.length];
+        count(a, aux, 0, a.length - 1);
+    }
+
+    private int count(int[] a, int[] aux, int lo, int hi) {
+        if (lo >= hi) {
+            return 0;
+        }
+
+        int mid = lo + (hi - lo) / 2;
+        return count(a, aux, lo, mid) + count(a, aux, mid + 1, hi) + 
+               merge(a, aux, lo, mid, hi);
+    }
+
+    private int merge(int[] a, int[] aux, int lo, int mid, int hi) {
+        // copy array in range
+        for (int i = lo; i <= hi; i++) {
+            aux[i] = a[i];
+        }
+
+        // merge two sorted array, counting inversions
+        int count = 0;
+        int i = lo; 
+        int j = mid + 1;
+        for (int k = lo; k <= hi; k++) {
+            if (i > mid) {
+                a[k] = aux[j++];
+            } else if (j > hi) {
+                a[k] = aux[i++];
+            } else if (aux[i].compareTo(aux[j]) > 0) {
+                a[k] = aux[j++];
+                count += mid - i + 1;
+            } else {
+                a[k] = aux[i++];
+            }
+        }
+    }
+}
