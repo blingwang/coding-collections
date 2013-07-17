1f07a23073f0bdd0d4ea8907246653a1839ab3ba
Edward <blingwang@gmail.com>
2013-07-17 17:59:36 -0500

    Update ValidParentheses.java

--- questions/ValidParentheses.java
+++ questions/ValidParentheses.java
@@ -5,24 +5,24 @@ public class ValidParentheses {
 		map.put(‘(‘, ‘)’);
 		map.put(‘[‘, ‘]’);
 		map.put(‘{‘, ‘}’);
-}
+	}
 
 	public boolean isValid(String s) {
 		ArrayDeque<Character> stack = new ArrayDeque<Character>();
-for (int i = 0; i < s.length(); i++) {
-	char c = s.charAt(i);
-	if (map.containsKey(c)) {
-		char left = map.get(c);
-		if (stack.isEmpty() || stack.pop() != left) return false;
-	} else {
-		stack.push(c);
-}
-}	
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (map.containsKey(c)) {
+				char left = map.get(c);
+				if (stack.isEmpty() || stack.pop() != left) return false;
+			} else {
+				stack.push(c);
+			}
+		}	
 		return stack.isEmpty();
-}
+	}
 
-public static void main(String[] args) {
-	ValidParentheses vp = new ValidParentheses();
-	assert(false == vp.isValid(“[[[“));	
-}
+	public static void main(String[] args) {
+		ValidParentheses vp = new ValidParentheses();
+		assert(false == vp.isValid(“[[[“));	
+	}
 }
