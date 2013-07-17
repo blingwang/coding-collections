ce46896a4cb25aa66946582e291d5856f0b6a579
Edward <blingwang@gmail.com>
2013-07-17 15:38:41 -0500

    Update Fibonacci.java

--- white-board/Fibonacci.java
+++ white-board/Fibonacci.java
@@ -16,6 +16,26 @@ public class Fibonacci {
         return f;
     }
     
+    public static void printFibs(int limit) {         
+        long[] series = new long[limit];
+       
+        //create first 2 series elements
+        series[0] = 0;
+        series[1] = 1;
+       
+        //create the Fibonacci series and store it in an array
+        for(int i=2; i < limit; i++){
+                series[i] = series[i-1] + series[i-2];
+        }
+       
+        //print the Fibonacci series numbers
+       
+        System.out.println("Fibonacci Series upto " + limit);
+        for(int i=0; i< limit; i++){
+                System.out.print(series[i] + " ");
+        }
+    }
+    
     public static long tailRecursive(long n) {
         if (n <= 2) {
             return 1;
