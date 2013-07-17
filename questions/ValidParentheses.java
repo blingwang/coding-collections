93e4cba7a19d0b1f68b9d83c44f2e877fd6d3d38
Edward <blingwang@gmail.com>
2013-07-17 17:56:14 -0500

    Update ValidParentheses.java

--- questions/ValidParentheses.java
+++ questions/ValidParentheses.java
@@ -1,28 +1,28 @@
 public class ValidParentheses {
-  private static final Map<Character, Character> map;
+	private static final Map<Character, Character> map;
 	static {
 		map = new HashMap<Character, Character>();
 		map.put(‘(‘, ‘)’);
 		map.put(‘[‘, ‘]’);
 		map.put(‘{‘, ‘}’);
-  }
+}
 
 	public boolean isValid(String s) {
 		ArrayDeque<Character> stack = new ArrayDeque<Character>();
-    for (int i = 0; i < s.length(); i++) {
-    	char c = s.charAt(i);
-    	if (map.containsKey(c)) {
-    		char left = map.get(c);
-    		if (stack.isEmpty() || stack.pop() != left) return false;
-    	} else {
-    		stack.push(c);
-      }
-    }	
+for (int i = 0; i < s.length(); i++) {
+	char c = s.charAt(i);
+	if (map.containsKey(c)) {
+		char left = map.get(c);
+		if (stack.isEmpty() || stack.pop() != left) return false;
+	} else {
+		stack.push(c);
+}
+}	
 		return stack.isEmpty();
 }
 
-  public static void main(String[] args) {
-  	ValidParentheses vp = new ValidParentheses();
-  	assert(false == vp.isValid(“[[[“));	
-  }
+public static void main(String[] args) {
+	ValidParentheses vp = new ValidParentheses();
+	assert(false == vp.isValid(“[[[“));	
+}
 }
