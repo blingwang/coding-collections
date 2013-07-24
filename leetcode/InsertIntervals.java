564938bb60bed9b01aea179777d3a3871dd9c076
Edward <blingwang@gmail.com>
2013-07-23 22:13:56 -0500

    Update InsertIntervals.java

--- leetcode/InsertIntervals.java
+++ leetcode/InsertIntervals.java
@@ -23,6 +23,55 @@ public class InsertIntervals {
        
        return mergedList;
    }
+   
+   public ArrayList<Interval> insertInplace(ArrayList<Interval> intervals, Interval newInterval) {
+        int insertPos = findInsertPosition(intervals, newInterval);
+        
+        int i = insertPos;
+        while(i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+            newInterval.start = Math.min(intervals.get(i).start, newInterval.start);
+            newInterval.end = Math.max(intervals.get(i).end, newInterval.end);
+            i++;
+        }
+        
+        if (i == insertPos) { // no overlap
+            intervals.add(insertPos, newInterval);
+        } else { // shift
+           intervals.set(insertPos, newInterval); 
+           int shift = i - insertPos - 1;
+           if (shift > 0) {
+               while (i < intervals.size()) {
+                   intervals.set(i-shift, intervals.get(i));
+                   i++;
+               }
+               while(shift > 0) {
+                   intervals.remove(intervals.size()-1);
+                   shift--;
+               }
+           }
+        }
+        
+        return intervals;
+    }
+    
+    private int findInsertPosition(ArrayList<Interval> intervals, Interval target) {
+        int lo = 0, hi = intervals.size() - 1;
+        int bestSoFar = intervals.size();
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (intervals.get(mid).end == target.start) {
+                bestSoFar = mid;
+                break;
+            } else if (intervals.get(mid).end > target.start) {
+                bestSoFar = mid;
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        
+        return bestSoFar;
+    }
 
    private class Interval {
        int start;
