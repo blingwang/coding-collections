4d8314f091f07f85b4fd17a2e29f4a47c38ec9ae
Edward <blingwang@gmail.com>
2013-07-25 17:59:11 -0500

    Update FirstMissingPositive.java

--- leetcode/FirstMissingPositive.java
+++ leetcode/FirstMissingPositive.java
@@ -2,11 +2,8 @@ public class FirstMissingPositive {
     public int firstMissingPositive(int[] A) {
         int n = A.length;
         
-        for( int i = 0; i < n; i++) {
-            if (A[i] <= 0 || A[i] > n) continue;
-            
-            while (A[i] != i + 1 && A[i] > 0 && 
-                   A[i] <= n && A[i] != A[A[i]-1]) {
+        for (int i = 0; i < n; i++) {
+            while (A[i] > 0 && A[i] <= n && A[i] != A[A[i]-1]) {
                 int temp = A[i];
                 A[i] = A[A[i]-1];
                 A[temp-1] = temp;
@@ -14,9 +11,9 @@ public class FirstMissingPositive {
         }
         
         for (int i = 0; i < n; i++) {
-            if (A[i] != i + 1) return i + 1;
+            if (A[i] != i+1) return i+1;
         }
         
-        return n + 1;
+        return n+1;
     }
 }
