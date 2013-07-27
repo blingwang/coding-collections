9dff8124c3293d36e9bd4da7db1214ad2618a863
Edward <blingwang@gmail.com>
2013-07-26 23:17:44 -0500

    Update UniquePaths.java

--- leetcode/UniquePaths.java
+++ leetcode/UniquePaths.java
@@ -1,4 +1,27 @@
 public class UniquePaths {
+    // Solution 1 using recursion + memorization
+    private int n;
+    private Map<Integer, Integer> cache; 
+    public int uniquePathsMemorization(int m, int n) {
+        this.n = n;
+        cache = new HashMap<Integer, Integer>();
+        return uniquePathsRecur(m-1, n-1);
+    }
+
+    private int uniquePathsRecur(int x, int y) {
+        if (x < 0 || y < 0) return 0;
+        if (x == 0 && y == 0) return 1;
+        
+        int key = x * n + y;  
+        if (cache.containsKey(key)) return cache.get(key);
+        
+        int pathCount = uniquePathsRecur(x-1, y) + uniquePathsRecur(x, y-1);
+    
+        cache.put(key, pathCount);
+        return pathCount;
+    }
+    
+    // Solution 4 using combinatorics 
     public int uniquePaths(int m, int n) {
         if (m < n) return uniquePaths(n, m);
         
