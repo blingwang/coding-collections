6ac6f426bae3be0b1606322f239badb5e95ddc16
Edward <blingwang@gmail.com>
2013-07-27 10:52:20 -0500

    Update MinPathSum.java

--- leetcode/MinPathSum.java
+++ leetcode/MinPathSum.java
@@ -24,4 +24,32 @@ public class MinPathSum {
         cache.put(key, minSum);
         return minSum;
     }
+    
+    // Solution 2 using DP: store in matrix
+    public int minPathSumDP2(int[][] grid) {
+        int m = grid.length;
+        if (m == 0) return 0;
+        int n = grid[0].length;
+        if (n == 0) return 0;
+        
+        int[][] pathSums = new int[m][n];
+        pathSums[0][0] = grid[0][0];
+        
+        for (int i = 1; i < m; i++) {
+            pathSums[i][0] = pathSums[i-1][0] + grid[i][0];
+        }
+        
+        for (int j = 1; j < n; j++) {
+            pathSums[0][j] = pathSums[0][j-1] + grid[0][j];
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                pathSums[i][j] = Math.min(pathSums[i-1][j], pathSums[i][j-1]) + 
+                                 grid[i][j];
+            }
+        }
+        
+        return pathSums[m-1][n-1];
+    }
 }
