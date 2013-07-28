0fb71aef433298f7693a7bbc1b606558b512dc89
Edward <blingwang@gmail.com>
2013-07-28 14:34:12 -0500

    Update Power.java

--- leetcode/Power.java
+++ leetcode/Power.java
@@ -1,4 +1,5 @@
 public class Power {
+    /* Solution 1 using recursion*/
     public double pow(double x, int n) {
         if (n == 0) return 1.0;
         
@@ -7,4 +8,30 @@ public class Power {
         else if (n > 0) return half * half * x;
         else            return half * half * (1/x);
     }
+    
+    /* Solution 2 using iteration*/
+    public double pow2(double x, int n) {
+        double result = 1.0;
+        while (n != 0) {
+            if ((n&1) == 1) {
+                if (n > 0) result *= x;
+                else       result *= 1/x;
+            } 
+            // x^(2k) = (x^2)^k;
+            x *= x;
+            n /= 2;
+        }
+        
+        return result;
+    }
+    
+     /* Solution 3 using iteration*/
+    public double pow3(double x, int n) {  
+        double result = 1;  
+        for (int m = Math.abs(n); m > 0; x *= x, m >>= 1) {  
+             if ((m & 1) == 1) result *= x;  
+        }  
+        
+        return (n >= 0) ? result : 1.0 / (result);  
+    }  
 }
