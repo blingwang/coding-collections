d4ed87a582c58b946f0c59fb5ec0309eafd6f14f
Edward <blingwang@gmail.com>
2013-07-28 12:36:40 -0500

    Update ThreeSum.java

--- leetcode/ThreeSum.java
+++ leetcode/ThreeSum.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class ThreeSum {
+    /* Solution 1 using two pointers*/
     public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         Arrays.sort(num);
@@ -33,5 +34,41 @@ public class ThreeSum {
             }
         }
         return result;
+    }
+    
+    /* Solution 2 using binary search */
+    public ArrayList<ArrayList<Integer>> threeSum2(int[] num) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+        Arrays.sort(num);
+        
+        for (int i = 0; i < num.length-2; i++) {
+            if (i > 0 && num[i] == num[i-1]) continue;
+            
+            for (int j = i+1; j < num.length-1; j++) {
+                if (j > i+1 && num[j] == num[j-1]) continue;
+                
+                int expected = -(num[i] + num[j]);
+                int k = binarySearch(num, j+1, num.length-1, expected);
+                if (k < 0) continue;
+                
+                ArrayList<Integer> triplet = new ArrayList<Integer>();
+                triplet.add(num[i]);
+                triplet.add(num[j]);
+                triplet.add(num[k]);
+                result.add(triplet);
+            }
+        }
+        
+        return result;
+    }
+    
+    private int binarySearch(int[] a, int lo, int hi, int target) {
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (a[mid] == target) return mid;
+            if (a[mid] < target) lo = mid + 1;
+            else                 hi = mid - 1;
+        }
+        return -1;
     }
 }
