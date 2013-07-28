6dc7980a69b470a0e4c95907e52b287ee442862d
Edward <blingwang@gmail.com>
2013-07-28 13:56:44 -0500

    Update Power.java

--- leetcode/Power.java
+++ leetcode/Power.java
@@ -5,6 +5,6 @@ public class Power {
         double half = pow(x, n/2);
         if (n % 2 == 0) return half * half;
         else if (n > 0) return half * half * x;
-        else return half * half * (1/x);
+        else            return half * half * (1/x);
     }
 }
