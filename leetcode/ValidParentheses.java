cb4406da14eea7431c6683400fbe5fcf9e4eefd3
Edward <blingwang@gmail.com>
2013-07-28 19:06:22 -0500

    Update ValidParentheses.java

--- leetcode/ValidParentheses.java
+++ leetcode/ValidParentheses.java
@@ -1,7 +1,30 @@
 import java.util.*;
 
 public class ValidParentheses {
+    private static final Map<Character, Character> map;
+    static {
+        map = new HashMap<Character, Character>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+    }
+    
     public boolean isValid(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!map.containsKey(c)) { // left parenthese
+                stack.push(c);
+            } else {
+                char left = map.get(c);
+                if (stack.isEmpty() || stack.pop() != left) return false;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+    
+    public boolean isValid2(String s) {
         ArrayDeque<Character> stack = new ArrayDeque<Character>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
