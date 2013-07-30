0e579b304a182cbd7ba9a7bd00e5e3caf5ba8737
Edward <blingwang@gmail.com>
2013-07-29 21:24:46 -0500

    Update SumRootToLeaf.java

--- leetcode/SumRootToLeaf.java
+++ leetcode/SumRootToLeaf.java
@@ -14,6 +14,36 @@ public class SumRootToLeaf {
         return sumNumbers(root.left, curSum) + sumNumbers(root.right, curSum); 
     }
 
+    // BFS
+    private int sumNumbersBFS(TreeNode root) {
+        if (root == null) return 0;
+        int sum = 0;
+        ArrayDeque<TreeNode> queue = new ArrayDeque<TreeNode>();
+        ArrayDeque<Integer> sumQueue = new ArrayDeque<Integer>();
+        queue.offer(root);
+        sumQueue.offer(root.val);
+        
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            int curSum = sumQueue.poll();
+            
+            if (cur.left == null && cur.right == null) sum += curSum;
+            
+            if (cur.left != null) {
+                queue.offer(cur.left);
+                sumQueue.offer(curSum * 10 + cur.left.val);
+            }
+            
+            if (cur.right != null) {
+                queue.offer(cur.right);
+                sumQueue.offer(curSum * 10 + cur.right.val);
+            }
+        }
+        
+        return sum;
+    }
+    
+    // DFS
     private int sum;
     public int sumNumbers2(TreeNode root) {
         sum = 0;
