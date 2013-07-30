cfa1f70721912f8c2f66b8740dcffda86b08add5
Edward <blingwang@gmail.com>
2013-07-29 22:12:33 -0500

    Update BinaryTreeLevelOrder.java

--- leetcode/BinaryTreeLevelOrder.java
+++ leetcode/BinaryTreeLevelOrder.java
@@ -1,8 +1,6 @@
 import java.util.*;
 
 public class BinaryTreeLevelOrder {
-    ArrayList<ArrayList<Integer>> result;
-
     public ArrayList<ArrayList<Integer>> levelOrder(TreeNode root) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         if (root == null) return result;
@@ -37,26 +35,28 @@ public class BinaryTreeLevelOrder {
 
         return result;
     }
-
+    
+    // DFS: T(k) = 2T(k-1) + c, k=lgn
+    // DFS use less memory since height is limited
+    // time complexity is still O(n): 1+3+7+...+2^k-1, k=lgn
     public ArrayList<ArrayList<Integer>> levelOrderDFS(TreeNode root) {
-        result = new ArrayList<ArrayList<Integer>>();
-        // DFS use less memory since height is limited
-        // time complexity is still O(n): 1+3+7+...+2^k-1, k=lgn
-        inOrder(root, 0); 
-        return result;
+        ArrayList<ArrayList<Integer>> levelList = new ArrayList<ArrayList<Integer>>();
+        dfs(root, levelList, 0);
+        return levelList;
     }
-
-    private void inOrder(TreeNode root, int level) { // T(k) = 2T(k-1) + c, k=lgn
+    
+    private void dfs(TreeNode root, ArrayList<ArrayList<Integer>> levelList, int level) {
         if (root == null) return;
-        if (level >= result.size()) {
-            ArrayList<Integer> levelNodes = new ArrayList<Integer>();
-            result.add(levelNodes);
+        
+        if (level >= levelList.size()) {
+            levelList.add(new ArrayList<Integer>());
         }
         
-        ArrayList<Integer> levelNodes = result.get(level);
-        levelNodes.add(root.val);
-        inOrder(root.left, level+1);
-        inOrder(root.right, level+1);
+        ArrayList<Integer> curLevel = levelList.get(level);
+        curLevel.add(root.val);
+        
+        dfs(root.left, levelList, level+1);
+        dfs(root.right, levelList, level+1);
     }
 
     private class TreeNode {
