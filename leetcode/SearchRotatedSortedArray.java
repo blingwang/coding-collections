ef925caa4c107f9c0971a642ee43376a3326c531
Edward <blingwang@gmail.com>
2013-07-31 11:24:35 -0500

    Update SearchRotatedSortedArray.java

--- leetcode/SearchRotatedSortedArray.java
+++ leetcode/SearchRotatedSortedArray.java
@@ -1,5 +1,24 @@
 public class SearchRotatedSortedArray {
     public int search(int[] A, int target) {
+        int lo = 0;
+        int hi = A.length - 1;
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (A[mid] == target) return mid;
+            if (A[hi] > A[mid]) { // right half sorted
+                if (target > A[mid] && target <= A[hi]) lo = mid + 1;
+                else hi = mid - 1;
+            } else { // left half sorted: use hi > mid since lo <= mid
+                if (target >= A[lo] && target < A[mid]) hi = mid -1;
+                else lo = mid + 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public int search2(int[] A, int target) {
         int lo = 0, hi = A.length - 1;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
@@ -14,4 +33,23 @@ public class SearchRotatedSortedArray {
         }
         return -1;
     }
+    
+    /* This problem is in fact the same as finding the minimum element's index.
+     * If the middle element is greater than the right most element, 
+     * then the pivot must be to the right; 
+     * if it is not, the pivot must be to the left.
+     */
+    int FindSortedArrayRotation(int A[], int N) {
+    int L = 0;
+    int R = N - 1;
+
+    while (A[L] > A[R]) {
+        int M = L + (R - L) / 2;
+        if (A[M] > A[R])
+            L = M + 1;
+        else
+            R = M;
+    }
+    return L;
+}
 }
