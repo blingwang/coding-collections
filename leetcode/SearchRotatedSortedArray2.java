f96fd9008c5b65b9d7718f22258975e66a313344
Edward <blingwang@gmail.com>
2013-08-03 01:01:35 -0500

    Update SearchRotatedSortedArray2.java

--- leetcode/SearchRotatedSortedArray2.java
+++ leetcode/SearchRotatedSortedArray2.java
@@ -3,16 +3,22 @@ public class SearchRotatedSortedArray2 {
         int lo = 0;
         int hi = A.length - 1;
         
+    public boolean search(int[] A, int target) {
+        int lo = 0;
+        int hi = A.length - 1;
+        
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            if (A[mid] == target) return true;
-            if (A[mid] < A[hi]) {// right half sorted
+            if (A[mid] == target) return true;         
+            
+            else if (A[mid] < A[hi]) {// right half sorted
                 if (target > A[mid] && target <= A[hi]) lo = mid + 1;
                 else hi = mid - 1;
             } else if (A[mid] > A[hi]) { // left half sorted
                 if (target >= A[lo] && target < A[mid]) hi = mid - 1;
                 else lo = mid + 1;
-            } else { // A[mid] == A[hi]
+            } else {
+                hi--; // A[mid] == A[hi]: move hi
             }
         }
         
