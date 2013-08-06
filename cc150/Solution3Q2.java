3a2fe2301b472feefcf4b81620d22ff090e56782
Edward <blingwang@gmail.com>
2013-08-06 18:47:45 -0500

    Update Solution3Q2.java

--- cc150/Solution3Q2.java
+++ cc150/Solution3Q2.java
@@ -1,30 +1,36 @@
 import java.util.*;
 
-public class Solution3Q2 extends Stack<Integer> {
-    Stack<Integer> s2;
+public class Solution3Q2 {
+    // use composition instead of inheritance
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
 
     public Solution3Q2() {
+        s1 = new Stack<Integer>();
         s2 = new Stack<Integer>();
     }
 
     public void push(int value){
+        s1.push(value);
+        
         if (value <= min()) {
             s2.push(value);
         }
-        super.push(value);
     }
 
     public Integer pop() {
-        int value = super.pop();
+        int value = s1.pop();
+        
         if (value == min()) {
             s2.pop();
         }
+        
         return value;
     }
 
     public int min() {
         if (s2.isEmpty()) {
-            return Integer.MAX_VALUE;
+            throw new RuntimeException("Empty Stack!");
         } else {
             return s2.peek();
         }
