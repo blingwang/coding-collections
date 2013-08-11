b66cc37ced0a56d11242bbbed3829f5937195cde
Edward <blingwang@gmail.com>
2013-08-10 21:47:04 -0500

    Update SwapNodePairs.java

--- leetcode/SwapNodePairs.java
+++ leetcode/SwapNodePairs.java
@@ -20,6 +20,25 @@ public class SwapNodePairs {
         return newHead;
     }
     
+    public ListNode swapPairs2(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        
+        while (cur.next != null && cur.next.next != null) {
+            ListNode first = cur.next;
+            ListNode second = cur.next.next;
+            
+            cur.next = second;
+            first.next = second.next;
+            second.next = first;
+            
+            cur = first; 
+        }
+        
+        return dummy.next;
+    }
+    
     public ListNode swapPairsRecursion(ListNode head) {
         if (head == null || head.next == null) return head;
         
