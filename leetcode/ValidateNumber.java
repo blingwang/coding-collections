bc2af63213ebaad96a4eba2441c5510e849fbbc9
Edward <blingwang@gmail.com>
2013-08-14 21:02:30 -0500

    Update ValidateNumber.java

--- leetcode/ValidateNumber.java
+++ leetcode/ValidateNumber.java
@@ -13,7 +13,7 @@ public class ValidateNumber {
         public int getType() { return type; }
     }
     
-    public boolean isNumber(String s) {
+    public boolean isNumber2(String s) {
         int[][] states = {
             {0, 8, -1, -1, 8, -1, -1, 8, 8},  
             {2, -1, -1, -1, -1, 6, -1, -1, -1},  
@@ -46,5 +46,63 @@ public class ValidateNumber {
         }
         
         return (state == 1 || state == 4 || state == 7 || state == 8);
+    }
+    
+    /*solution 3*/
+    public boolean isNumber3(String s) {
+        s = s.trim();
+        
+        int i = s.indexOf('e');
+        if (i == -1) {
+            return isDouble(s);
+        } else {
+            return isDouble(s.substring(0,i)) && isInteger(s.substring(i+1));
+        }
+    }
+    
+    private boolean isDouble(String s) {
+        if (s.startsWith("+") || s.startsWith("-")) {
+            return isUnsignedDouble(s.substring(1));
+        } else {
+            return isUnsignedDouble(s);
+        }
+    }
+    
+    private boolean isUnsignedDouble(String s) {
+        int i = s.indexOf('.');
+        if (i == -1) {
+            return isUnsignedInteger(s);
+        } else {
+            if (s.length() == 1) {// "." => false
+                return false;
+            }
+            
+            String left = s.substring(0, i);
+            String right = s.substring(i+1);
+            return (left.equals("") || isUnsignedInteger(left)) &&
+                   (right.equals("") || isUnsignedInteger(right));
+        }
+    }
+    
+    private boolean isInteger(String s) {
+        if (s.startsWith("+") || s.startsWith("-")) {
+            return isUnsignedInteger(s.substring(1));
+        } else {
+            return isUnsignedInteger(s);
+        }
+    }
+    
+    private boolean isUnsignedInteger(String s) {
+        if (s.length() == 0) {
+            return false;
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isDigit(s.charAt(i))) {
+                return false;
+            }
+        }
+        
+        return true;
     }
 }
