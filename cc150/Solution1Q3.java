1841ad2ec83be8487afefc70739728a9a7dfe7ae
Edward <blingwang@gmail.com>
2013-08-18 18:20:20 -0500

    Update Solution1Q3.java

--- cc150/Solution1Q3.java
+++ cc150/Solution1Q3.java
@@ -18,30 +18,23 @@ public class Solution1Q3{
 
     // solution 2
     public boolean isAnagram2(String s, String t){
-        if(s.length() != t.length()){
-            return false;
-        }
-
-        int[] letters = new int[256];// assumption
-        
-        char[] sArray = s.toCharArray();
-        for(char c : sArray){// count number of each char in s
-            letters[c]++;
-        }
-
-        // no need to put t in a char array
-        for(int i = 0; i < t.length(); i++){
-            int c = (int) t.charAt(i);
-
-            // only need to test if count of c is 0 to 
-            // compare two strings with equal length
-            // at least one extra char in t, not in s
-            if(--letters[c] < 0){
-                return false;
-            }
-        }
-
-        return true;
+        if (s.length() != t.length()) {
+			return false;
+		}
+		
+		int[] charCount = new int[256];
+		
+		for (int i = 0; i < s.length(); i++) {
+			charCount[s.charAt(i)]++;
+		}
+		
+		for (int i = 0; i < t.length(); i++) {
+			charCount[t.charAt(i)]--; // consume chars in s
+			if (charCount[t.charAt(i)] < 0) {
+				return false;
+			}
+		}
+		return true;
     }
 }
 
