682ebd72d831aad6bae58e0d2fe6b00cc4476a90
Edward <blingwang@gmail.com>
2013-08-18 18:20:44 -0500

    Update Solution1Q3.java

--- cc150/Solution1Q3.java
+++ cc150/Solution1Q3.java
@@ -19,22 +19,22 @@ public class Solution1Q3{
     // solution 2
     public boolean isAnagram2(String s, String t){
         if (s.length() != t.length()) {
+		return false;
+	}
+	
+	int[] charCount = new int[256];
+	
+	for (int i = 0; i < s.length(); i++) {
+		charCount[s.charAt(i)]++;
+	}
+	
+	for (int i = 0; i < t.length(); i++) {
+		charCount[t.charAt(i)]--; // consume chars in s
+		if (charCount[t.charAt(i)] < 0) {
 			return false;
 		}
-		
-		int[] charCount = new int[256];
-		
-		for (int i = 0; i < s.length(); i++) {
-			charCount[s.charAt(i)]++;
-		}
-		
-		for (int i = 0; i < t.length(); i++) {
-			charCount[t.charAt(i)]--; // consume chars in s
-			if (charCount[t.charAt(i)] < 0) {
-				return false;
-			}
-		}
-		return true;
+	}
+	return true;
     }
 }
 
