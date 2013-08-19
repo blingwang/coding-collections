dfd4df0dd6eaa8c64cf34b5ad1fa74de5dc10b68
Edward <blingwang@gmail.com>
2013-08-19 00:14:57 -0500

    Update Solution1Q2.c

--- cc150/Solution1Q2.c
+++ cc150/Solution1Q2.c
@@ -1,18 +1,19 @@
 void reverse(char *str){
+    if (!str) return;
+    
     char* end = str;
     char tmp;
-    if(str){
-        while(*end){// find end of the string
-            ++end;
-        }
-        --end; // set one char back, since last char is null
+    
+    while(*end){// find end of the string
+        ++end;
+    }
+    --end; // set one char back, since last char is null
 
-        // swap characters from start of string with the end 
-        // of the string, untill the pointers meet in the middle
-        while(str < end){
-            tmp = *str;
-            *str++ = *end;
-            *end++ = tmp;
-        }
+    // swap characters from start of string with the end 
+    // of the string, untill the pointers meet in the middle
+    while(str < end){
+        tmp = *str;
+        *str++ = *end;
+        *end++ = tmp;
     }
 }
