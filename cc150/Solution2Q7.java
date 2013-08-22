6e21bdfe0028ab3104058fb496a6fccbe7f88df5
Edward <blingwang@gmail.com>
2013-08-22 08:58:37 -0500

    Update Solution2Q7.java

--- cc150/Solution2Q7.java
+++ cc150/Solution2Q7.java
@@ -1,92 +1,97 @@
 import java.util.*;
 
-public class Solution2Q7{
-    class LinkedListNode{
-        LinkedListNode next = null;
-        int data;
-
-        public LinkedListNode(int d){
-            data = d;
-        }
-    }
-    
-    boolean isPalindrome(LinkedListNode head) {
-        LinkedListNode fast = head;
-        LinkedListNode slow = head;
-
-        Stack<Integer> stack = new Stack<Integer>();
-
-        // push elements from first half of linked list onto stack.
-        // when fast runner reaches the end of the linked list, 
-        // then we know slow is at the middle
-        while(fast != null && fast.next != null) {
-            stack.push(slow.data);
+public class Solution {
+    public static boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ArrayDeque<Integer> stack = new ArrayDeque<Integer>();
+        
+        // push first half on to stack
+        while (fast != null && fast.next != null) {
+            stack.push(slow.val);
             slow = slow.next;
             fast = fast.next.next;
         }
-
-        // has odd number of elements, so skip the middle element
-        if (fast != null) {
+        
+        if (fast != null) {//skip mid for list with odd # of nodes
             slow = slow.next;
         }
-
-        while (slow != null){
-            int top = stack.pop();
-
-            // if values are different, then it's not a palindrome
-            if (top != slow.data) {
+        
+        // check second half against fisrt half
+        while (slow != null) {
+            if (slow.val != stack.pop()) {
                 return false;
             }
-
             slow = slow.next;
         }
-
+        
         return true;
     }
-
-    // recursive approach
-    class Result{
-        public LinkedListNode node;
-        public boolean result;
-
-        Result(LinkedListNode n, boolean r) {
-            node = n;
-            result = r;
-        }
+    
+    public static boolean isPalindrome2(ListNode head) {
+        if (head == null) return true;
+        return isPalindromeRecurse(head, length(head)).isPalindrome;
     }
-
-    Result isPalindromeRecurse(LinkedListNode head, int length) {
-        if(head == null || length == 0){
-            return new Result(null, true);
-        } else if (length == 1) {
+    
+    private static Result isPalindromeRecurse(ListNode head, int length) {
+        if (length == 1) {
             return new Result(head.next, true);
-        } else if (length == 2) {
-            return new Result(head.next.next, head.data == head.next.data);
         }
-
-        Result res = isPalindromeRecurse(head.next, length -2);
-        if(!res.result || res.node == null) {
-            return res;
-        } else {
-            res.result = head.data == res.node.data;
-            res.node = res.node.next;
-            return res;
+        
+        if (length == 2) {
+            return new Result(head.next.next, head.val == head.next.val);
+        }
+        
+        // recurse
+        Result result = isPalindromeRecurse(head.next, length - 2);
+        
+        if (!result.isPalindrome) { // shortcut
+            return result;
+        }
+        
+        return new Result(result.next, head.val == result.next.val);
+    }
+    
+    private static int length(ListNode head) {
+        int length = 0;
+        for (ListNode cur = head; cur != null; cur = cur.next) {
+            length++;
+        }
+        return length;
+    }
+	
+    public static void main(String[] args) {
+    	ListNode head = new ListNode(3);
+    	head.next = new ListNode(2);
+    	head.next.next = new ListNode(3);
+    	
+    	ListNode cur = head;
+        while (cur != null) {
+            System.out.println(cur.val);
+            cur = cur.next;
+        }
+        
+        System.out.println(isPalindrome2(head));
+    }
+    
+    /**
+     * Auxiliary class for isPalindromeRecurse 
+     */
+    private static class Result {
+        boolean isPalindrome;
+        ListNode next;
+        Result(ListNode next, boolean isPalindrome) {
+            this.isPalindrome = isPalindrome;
+            this.next = next;
         }
     }
-
-    boolean isPalindromeRecurse(LinkedListNode head) {
-        Result p = isPalindromeRecurse(head, listSize(head));
-        return p.result;
-    }
-
-    private int listSize(LinkedListNode head) {
-        int size = 0;
-
-        while(head != null){
-            size++;
-            head = head.next;
+    
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
         }
-
-        return size;
     }
 }
