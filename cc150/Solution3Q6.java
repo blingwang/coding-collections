8faa2ac6999b620cd6330984f56dc857f3674ee1
Edward <blingwang@gmail.com>
2013-08-23 08:47:16 -0500

    Update Solution3Q6.java

--- cc150/Solution3Q6.java
+++ cc150/Solution3Q6.java
@@ -1,15 +1,32 @@
 import java.util.*;
-
-class Solution3Q6 {
-    public static Stack<Integer> sort(Stack<Integer> s) {
-        Stack<Integer> r = new Stack<Integer>();
-        while (!s.isEmpty()) {
-            int tmp = s.pop();
-            while (!r.isEmpty() && r.peek() < tmp) {
-                s.push(r.pop());
+public class Solution3Q6 {
+    public static void sortStack(ArrayDeque<Integer> stack) {
+        ArrayDeque<Integer> aux = new ArrayDeque<Integer>();
+        
+        while (!stack.isEmpty()) {
+            int top = stack.pop();
+            while (!aux.isEmpty() && top > aux.peek()) {
+                stack.push(aux.pop());
             }
-            r.push(tmp);
+            aux.push(top);
         }
-        return r;
+        
+        // push back to original stack
+        while (!aux.isEmpty()) {
+            stack.push(aux.pop());
+        }
+    }
+    
+    public static void main(String[] args) {
+        ArrayDeque<Integer> stack = new ArrayDeque<Integer>();
+        stack.push(6);
+        stack.push(8);
+        stack.push(1);
+        stack.push(3);
+        sortStack(stack);
+        
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
     }
 }
