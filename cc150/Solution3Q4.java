4037dadeac337181b407867ebdb50a7a7619a82a
Edward <blingwang@gmail.com>
2013-08-23 00:58:02 -0500

    Update Solution3Q4.java

--- cc150/Solution3Q4.java
+++ cc150/Solution3Q4.java
@@ -1,53 +1,70 @@
 import java.util.*;
 public class Solution3Q4 {
-    static class Tower {
-        private Stack<Integer> disks;
-        private int index;
-
-        public Tower(int i) {
-            disks = new Stack<Integer>();
-            index = i;
-        }
-
-        public int index() {
-            return index;
-        }
-
-        public void add(int d) {
-            if(!disks.isEmpty() && disks.peek() <= d) {
-                System.out.println("Error placing disk " + d);
-            } else {
-                disks.push(d);
-            }
-        }
-
-        public void moveTopTo(Tower t) {
-            int top = disks.pop();
-            t.add(top);
-            System.out.println("Move disk " + top + " from " + index() + " to " + t.index());
-        }
-
-        public void moveDisks(int n, Tower destination, Tower buffer) {
-            if (n > 0) {
-                moveDisks(n-1, buffer, destination);
-                moveTopTo(destination); 
-                buffer.moveDisks(n-1, destination, this);
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        int n = 3;
-        Tower[] towers = new Tower[n];
+        HanoiTower ht = new HanoiTower(3);
+        ht.play();
+    }
+}
 
+class HanoiTower {
+    private final int n;
+    private Tower[] towers;
+    
+    public HanoiTower(int n) {
+        this.n = n;
+        towers = new Tower[3];
+        
         for (int i = 0; i < 3; i++) {
             towers[i] = new Tower(i);
         }
-
-        for (int i = n - 1; i >= 0; i--) {
-            towers[0].add(i);
+        
+        for (int i = n-1; i >= 0; i--) {
+            towers[0].push(i);
         }
-
-        towers[0].moveDisks(n, towers[2], towers[1]);
+    }
+    
+    public void play() {
+        moveDisks(3, 0, 2, 1);
+    }
+    
+    public void moveDisks(int n, int from, int to, int buffer) {
+        if (n== 0) return;
+        
+        moveDisks(n-1, from, buffer, to);
+        moveTop(from, to);
+        moveDisks(n-1, buffer, to, from);
+    }
+    
+    public void moveTop(int from, int to) {
+        int disk = towers[from].pop();
+        towers[to].push(disk);
+        System.out.println("Move disk " + disk + " from " + towers[from].index() +
+                            " to " + towers[to].index());
+    }
+    
+    private static class Tower {
+        private int index;
+        private ArrayDeque<Integer> disks;
+        
+        public Tower(int index) {
+            this.index = index;
+            disks = new ArrayDeque<Integer>();
+        }
+        
+        public int index() {
+            return index;
+        }
+        
+        public void push(int i) {
+            if (!disks.isEmpty() && i >= disks.peek()) {
+                throw new RuntimeException("Error placing " + i + " to Tower " + index());
+            } 
+            
+            disks.push(i);
+        }
+        
+        public int pop() {
+            return disks.pop();
+        }
     }
 }
