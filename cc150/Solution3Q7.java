f2fab85284608b66ecfb01a24ef249ce556441d7
Edward <blingwang@gmail.com>
2013-08-24 00:18:30 -0500

    Update Solution3Q7.java

--- cc150/Solution3Q7.java
+++ cc150/Solution3Q7.java
@@ -3,12 +3,13 @@ public class Solution3Q7 {
     private abstract class Animal {
         private int order;
         protected String name;
-        public Animal(String n) {
-            name = n;
+        
+        public Animal(String name) {
+            this.name = name;
         }
 
-        public void setOrder(int ord) {
-            order = ord;
+        public void setOrder(int order) {
+            this.order = order;
         }
 
         public int getOrder() {
@@ -21,40 +22,35 @@ public class Solution3Q7 {
     }
 
     private class Dog extends Animal {
-        public Dog(String n) {
-            super(n);
+        public Dog(String name) {
+            super(name);
         }
     }
 
     private class Cat extends Animal {
-        public Cat(String n) {
-            super(n);
+        public Cat(String name) {
+            super(name);
         }
     }
 
-    LinkedList<Dog> dogs = new LinkedList<Dog>();
-    LinkedList<Cat> cats = new LinkedList<Cat>();
+    Queue<Dog> dogs = new LinkedList<Dog>();
+    Queue<Cat> cats = new LinkedList<Cat>();
     private int order = 0; // acts as timestamp
 
-    public void enqueue(Animal a) {
-        // order is used as a sort of timestamp, so that we can compare 
-        // the insertion order of a dog to a cat
-        a.setOrder(order);
-        order++;
-
-        // I would prefer overloading enqueue with diff types to using instanceof
-        if (a instanceof Dog) {
-            dogs.addLast((Dog) a);
-        } else if (a instanceof Cat) {
-            cats.addLast((Cat) a);
-        }
+    public void enqueue(Dog dog) {// polymorphism by overloading
+        dog.setOrder(order++);
+        dogs.offer(dog);
+    }
+    
+    public void enqueue(Cat cat) {
+        cat.setOrder(order++);
+        cats.offer(cat);
     }
 
     public Animal dequeueAny() {
-        // look at tops of dog and cat queues, and dequeue the oldest one
-        if (dogs.size() == 0) {
-            return cats.poll(); // pop
-        } else if (cats.size() == 0) {
+        if (dogs.isEmpty()) {
+            return cats.poll();
+        } else if (cats.isEmpty()) {
             return dogs.poll();
         }
 
@@ -69,11 +65,14 @@ public class Solution3Q7 {
     }
 
     public Dog dequeueDogs() {
-        return dogs.getFirst();
+        return dogs.poll();
     }
 
     public Cat dequeueCats() {
-        return cats.getFirst();
+        return cats.poll();
+    }
+    
+    public static void main(String[] args) {
+        
     }
 }
-
