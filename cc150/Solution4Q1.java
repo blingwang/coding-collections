a927f600c599f37cafff5b2c707fa0c302cf5ac6
Edward <blingwang@gmail.com>
2013-08-24 09:31:36 -0500

    Update Solution4Q1.java

--- cc150/Solution4Q1.java
+++ cc150/Solution4Q1.java
@@ -1,62 +1,52 @@
-public class Solution4Q1 {
-    private class TreeNode {
-        int data;
+public class Solution4Q1 {   
+    public static boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        
+        if (Math.abs(left - right) > 1) return false;
+        
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+    
+    private static int getHeight(TreeNode root) {
+        if (root == null) return 0;
+       
+        return Math.max(getHeight(root.left), getHeight(root.left)) + 1;
+    }
+    
+    public static boolean isBalancedBU(TreeNode root) {
+        return checkHeight(root) > 0;
+    }
+    
+    private static int checkHeight(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = checkHeight(root.left);
+        if (left < 0) return -1;
+        
+        int right = checkHeight(root.right);
+        if (right < 0) return -1;
+        
+        if (Math.abs(left - right) > 1) return -1;
+        
+        return Math.max(left, right) + 1;
+    }
+    
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        
+        System.out.println(isBalancedBU(root));
+    }
+    private static class TreeNode {
+        int val;
         TreeNode left;
         TreeNode right;
-
-        public TreeNode(int data) {
-            this.data = data;
+        TreeNode(int x) {
+            val = x;
         }
     }
-
-    private static int getHeight(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-
-        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
-    }
-
-    public static boolean isBalanced1(TreeNode root) {
-        if (root == null) {
-            return true;
-        }
-
-        int heightDiff = getHeight(root.left) - getHeight(root.right);
-        if (Math.abs(heightDiff) > 1) {
-            return false;
-        } else {// recur: good to have tail recursion
-            return isBalanced1(root.left) && isBalanced1(root.right);
-        }
-    }
-
-    public static int checkHeight(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-
-        // check if left is balanced
-        int leftHeight = checkHeight(root.left);
-        if (leftHeight == -1) {
-            return -1;
-        }
-
-        // check if right is balanced
-        int rightHeight = checkHeight(root.right);
-        if (rightHeight == -1) {
-            return -1;
-        }
-
-        // check if current node is balanced
-        int heightDiff = leftHeight - rightHeight;
-        if (Math.abs(heightDiff) > 1) {
-            return -1;
-        } else {
-            return Math.max(leftHeight, rightHeight) + 1;
-        }
-    }
-
-    public static boolean isBalanced2(TreeNode root) {
-       return checkHeight(root) == -1;
-    }
-} 
+}
