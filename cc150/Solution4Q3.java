5eaa03eb1ee157ebbeaa0a73ebf822102264e1f0
Edward <blingwang@gmail.com>
2013-08-24 11:44:45 -0500

    Update Solution4Q3.java

--- cc150/Solution4Q3.java
+++ cc150/Solution4Q3.java
@@ -1,29 +1,46 @@
-public class Solution4Q3 {
-    private class TreeNode {
-        int data;
+import java.util.*;
+
+public class Solution4Q3 {   
+    public static TreeNode sortedArrayToBST(int[] a) {
+        return sortedArrayToBST(a, 0, a.length - 1);
+    }
+    
+    private static TreeNode sortedArrayToBST(int[] a, int lo, int hi) {
+        if (lo > hi) return null;
+        
+        int mid = lo + (hi - lo) / 2;
+        TreeNode root = new TreeNode(a[mid]);
+        
+        root.left = sortedArrayToBST(a, lo, mid - 1);
+        root.right = sortedArrayToBST(a, mid + 1, hi);
+        
+        return root;
+    }
+    
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        
+        int[] a = {1, 2, 3, 4, 5, 6, 7};
+        TreeNode bst = sortedArrayToBST(a);
+        bst.print();
+        
+        System.out.println();
+    }
+    
+    private static class TreeNode {
+        int val;
         TreeNode left;
         TreeNode right;
-
-        public TreeNode(int data) {
-            this.data = data;
+        TreeNode(int x) {
+            val = x;
         }
-    }
-
-    TreeNode createMinimalBST(int[] arr, int start, int end) {
-        if (end < start) {
-            return null;
+        
+        public void print() {
+            System.out.print(val);
+            if (left != null) left.print();
+            if (right != null) right.print();
         }
-
-        int mid = (start + end) / 2;
-        TreeNode n = new TreeNode(arr[mid]);
-
-        n.left = createMinimalBST(arr, start, mid - 1);
-        n.right = createMinimalBST(arr, mid + 1, end);
-
-        return n;
-    }
-
-    TreeNode CreateMinimalBST(int[] arr) {
-        return createMinimalBST(arr, 0, arr.length -1);
     }
 }
