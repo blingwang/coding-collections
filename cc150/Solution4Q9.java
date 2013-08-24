b2c7b9474691c693f7db8c590a955275833c2783
Edward <blingwang@gmail.com>
2013-08-24 18:51:48 -0500

    Update Solution4Q9.java

--- cc150/Solution4Q9.java
+++ cc150/Solution4Q9.java
@@ -1,58 +1,78 @@
-public class Solution4Q9 {
-    private class TreeNode {
-        int data;
-        TreeNode left;
-        TreeNode right;
-        TreeNode parent;
-
-        public TreeNode(int data) {
-            this.data = data;
-        }
+public class Solution4Q9 { 
+    public static void findSum(TreeNode root, int sum) {
+        int[] path = new int[depth(root)];
+        findSum(root, sum, path, 0);
     }
-
-    public void findSum(TreeNode node, int sum, int[] path, int level) {
-        if (node == null) {
-            return;
-        }
-
-        // insert current node into path
-        path[level] = node.data;
-
-        // look for paths with a sum that ends at this node
-        int t = 0;
-        for (int i = level; i >= 0; i--) {
-            t += path[i];
-            if (t == sum) {
-                print(path, i, level);
+    
+    private static void findSum(TreeNode root, int sum, int[] path, int depth) {
+        if (root == null) return;
+        
+        path[depth] = root.val;
+        int curSum = 0;
+        for (int i = depth; i >= 0; i--) {
+            curSum += path[i];
+            if (curSum == sum) {
+                printPath(path, i, depth);
             }
         }
-
-        //search nodes beneath this one
-        findSum(node.left, sum, path, level + 1);
-        findSum(node.right, sum, path, level + 1);
-
-        // not strictly necessary
-        path[level] = Integer.MIN_VALUE;
+        
+        findSum(root.left, sum, path, depth + 1);
+        findSum(root.right, sum, path, depth + 1);
     }
-
-    public void findSum(TreeNode node, int sum) {
-        int depth = depth(node);
-        int[] path = new int[depth];
-        findSum(node, sum, path, 0);
-    }
-
-    public void print(int[] path, int start, int end) {
+    
+    private static void printPath(int[] path, int start, int end) {
+        assert (start >= 0 && end < path.length && start <= end);
         for (int i = start; i <= end; i++) {
             System.out.print(path[i] + " ");
         }
         System.out.println();
     }
-
-    public int depth(TreeNode node) {
-        if (node == null) {
-            return 0;
+    
+    private static int depth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+    
+    public static TreeNode sortedArrayToBST(int[] a) {
+        return sortedArrayToBST(a, 0, a.length - 1);
+    }
+    
+    private static TreeNode sortedArrayToBST(int[] a, int lo, int hi) {
+        if (lo > hi) return null;
+        
+        int mid = lo + (hi - lo) / 2;
+        TreeNode root = new TreeNode(a[mid]);
+        
+        root.left = sortedArrayToBST(a, lo, mid - 1);
+        root.right = sortedArrayToBST(a, mid + 1, hi);
+        
+        return root;
+    }
+    
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        
+        int[] a = {1, 2, 3, 4, 5, 6, 7};
+        TreeNode bst = sortedArrayToBST(a);
+        bst.print();   
+        System.out.println();
+        findSum(bst, 7);
+    }
+    
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
         }
-
-        return 1 + Math.max(depth(node.left), depth(node.right));
+        
+        public void print() {
+            System.out.print(val + " ");
+            if (left != null) left.print();
+            if (right != null) right.print();
+        }
     }
 }
