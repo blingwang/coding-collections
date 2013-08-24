4a305c0a5977afdd0610ca61541b732198455b31
Edward <blingwang@gmail.com>
2013-08-24 15:19:25 -0500

    Update Solution4Q5.java

--- cc150/Solution4Q5.java
+++ cc150/Solution4Q5.java
@@ -1,48 +1,66 @@
-public class Solution4Q5 {
-    private class TreeNode {
-        int data;
+public class Solution4Q5 { 
+    public static boolean isBST(TreeNode root) {
+//        TreeNode pre = new TreeNode(Integer.MIN_VALUE);
+//        return isBSTInOrder(root, pre);
+        return inRange(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    
+    private static boolean isBSTInOrder(TreeNode root, TreeNode pre) {
+        if (root == null) return true;
+        
+        if (!isBSTInOrder(root.left, pre)) return false;       
+        if (pre.val >= root.val) return false;
+        pre.val = root.val; // don't pre = root: pre is local var   
+        if (!isBSTInOrder(root.right, pre)) return false;
+        
+        return true;
+    }
+    
+    private static boolean inRange(TreeNode root, int min, int max) {
+        if (root == null) return true;
+        if (root.val < min || root.val > max) return false;
+        return inRange(root.left, min, root.val) && inRange(root.right, root.val, max);
+    }
+    
+    public static TreeNode sortedArrayToBST(int[] a) {
+        return sortedArrayToBST(a, 0, a.length - 1);
+    }
+    
+    private static TreeNode sortedArrayToBST(int[] a, int lo, int hi) {
+        if (lo > hi) return null;
+        
+        int mid = lo + (hi - lo) / 2;
+        TreeNode root = new TreeNode(a[mid]);
+        
+        root.left = sortedArrayToBST(a, lo, mid - 1);
+        root.right = sortedArrayToBST(a, mid + 1, hi);
+        
+        return root;
+    }
+    
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        
+        int[] a = {1, 2, 3, 4, 5, 6, 7};
+        TreeNode bst = sortedArrayToBST(a);
+        bst.print();      
+        System.out.println(isBST(bst));
+    }
+    
+    private static class TreeNode {
+        int val;
         TreeNode left;
         TreeNode right;
-
-        public TreeNode(int data) {
-            this.data = data;
+        TreeNode(int x) {
+            val = x;
         }
-    }
-
-    public static int last_printed = Integer.MIN_VALUE;
-    public static boolean inOrderCheckBST(TreeNode n) {
-        if(n == null) return true;
-
-        // check left
-        if (!inOrderCheckBST(n.left)) return false;
-
-        // check current
-        if (n.data < last_printed) return false;
-        last_printed = n.data;
-
-        // check right
-        if (!inOrderCheckBST(n.right)) return false;
-
-        return true; // all good
-    }
-
-    public boolean checkBST(TreeNode n) {
-        return checkBST(n, Integer.MIN_VALUE, Integer.MAX_VALUE);
-    }
-
-    private boolean checkBST(TreeNode n, int min, int max) {
-        if (n == null) {
-            return true;
+        
+        public void print() {
+            System.out.print(val);
+            if (left != null) left.print();
+            if (right != null) right.print();
         }
-
-        if(n.data <= min || n.data > max) {
-            return false;
-        }
-
-        if (!checkBST(n.left, min, n.data) || !checkBST(n.right, n.data, max)) {
-            return false;
-        }
-
-        return true;
     }
 }
