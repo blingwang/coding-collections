07e584621ba95afab95d2008c4265707f4b27bd3
Edward <blingwang@gmail.com>
2013-08-25 16:57:05 -0500

    Update Solution9Q9.java

--- cc150/Solution9Q9.java
+++ cc150/Solution9Q9.java
@@ -1,33 +1,56 @@
 import java.util.*;
-class Solution9Q9 {
-    int GRID_SIZE = 8;
 
-    void placeQueens(int row, int[] columns, ArrayList<int[]> results) {
-        if (row == GRID_SIZE) { // Found valid placement
-            results.add(columns.clone());
-        } else {
-            for (int col = 0; col < GRID_SIZE; col++) {
-                if (checkValid(columns, row, col)) {
-                    columns[row] = col; // Place queen
-                    placeQueens(row + 1, columns, results);
-                }
-            }
+public class Solution9Q9 { 
+    private int N = 8;
+
+    public ArrayList<int[]> solveNQueens() {
+        ArrayList<int[]> solutions = new ArrayList<int[]>();
+        int[] a = new int[N];
+        
+        for (int i = 0; i < N; i++) {
+            a[i] = i;
+        }
+        
+        placeQueens(a, solutions, 0);
+        return solutions;
+    }
+    
+    private void placeQueens(int[] a, ArrayList<int[]> solutions, int k) {
+        if (k == N) {
+            solutions.add(Arrays.copyOf(a, a.length));
+            return;
+        }
+        
+        for (int i = k; i < N; i++) {
+            swap(a, k, i);
+            if (isValid(a, k)) placeQueens(a, solutions, k+1);
+            swap(a, k, i);
         }
     }
+    
+    private void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
 
-   boolean checkValid(int[] columns, int row, int column) {
-        for (int oldRow = 0; oldRow < row; oldRow++) {
-            int oldColumn = columns[oldRow];
-            if (oldColumn == column) { // same column
-               return false;
-            }
-
-            int columnDistance = Math.abs(oldColumn - column);
-            int rowDistance = row - oldRow;
-            if (columnDistance == rowDistance) { // same diagonal
+    private boolean isValid(int[] a, int k) {
+        for (int i = 0; i < k; i++) { // row distance == col distance
+            if (a[k] - a[i] == k - i || a[i] - a[k] == k - i) {
                 return false;
             }
         }
+        
         return true;
-   } 
-} 
+    }
+    
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        for (int[] a : sol.solveNQueens()) {
+            for (int i : a) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
