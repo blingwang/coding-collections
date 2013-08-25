ad795687140f3d122ce89068ba51504e12ff2b1a
Edward <blingwang@gmail.com>
2013-08-24 21:48:49 -0500

    Update Solution9Q1.java

--- cc150/Solution9Q1.java
+++ cc150/Solution9Q1.java
@@ -1,21 +1,41 @@
 class Solution9Q1 {
-    public static int countWaysDP(int n, int[] map) {
-        if (n < 0) {
-            return 0;
-        } else if (n == 0) { // all steps hopped over, got one way
-            return 1;
-        } else if (map[n] > -1) {
-            return map[n];
-        } else {
-            map[n] = countWaysDP(n - 1, map) + 
-                     countWaysDP(n - 2, map) +
-                     countWaysDP(n - 3, map);
-           return map[n];
+    public static int countWays(int n) {// similar to Fib  #
+        int oneStepBack = 2; // n = 2
+        int twoStepBack = 1; // n = 1
+        int threeStepBack = 1; // n = 0;
+        
+        for (int i = 3; i <= n; i++) {
+            int cur = oneStepBack + twoStepBack + threeStepBack;
+            threeStepBack = twoStepBack;
+            twoStepBack = oneStepBack;
+            oneStepBack = cur;
         }
+        
+        return oneStepBack;
     }
 
-    public static int countWays(int n) {
-        int[] map = new int[n+1];
-        return countWaysDP(n, map);
+    public static int countWays2(int n) {
+        int[] cache = new int[n+1];
+        for (int i = 0; i < cache.length; i++) {
+            cache[i] = -1;
+        }
+        
+        return countWaysDP(n, cache);
+    }
+    
+    public static int countWaysDP(int n, int[] cache) {
+        if (cache[n] >= 0) return cache[n];
+        if (n < 0) return 0;
+        if (n == 0) return 1;// all steps hopped over, got one way
+        
+        cache[n] = countWaysDP(n - 1, cache) + 
+                   countWaysDP(n - 2, cache) + 
+                   countWaysDP(n - 3, cache);
+        
+        return cache[n];
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(countWays(3));
     }
 } 
