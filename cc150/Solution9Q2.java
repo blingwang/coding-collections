2c189bf4fdd0f0b5b94f5180619c901145b01790
Edward <blingwang@gmail.com>
2013-08-25 16:31:13 -0500

    Update Solution9Q2.java

--- cc150/Solution9Q2.java
+++ cc150/Solution9Q2.java
@@ -2,6 +2,36 @@ import java.util.*;
 class Solution9Q2 {
     private Random rand = new Random();
     
+    // count paths: 0 for reee; 1 for obstacle
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        if (m == 0) return 0;
+        int n = obstacleGrid[0].length;
+        if (n == 0) return 0;
+        
+        int[][] pathCounts = new int[m][n];
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0; 
+        pathCounts[0][0] = 1;
+        
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] != 1) pathCounts[i][0] = pathCounts[i-1][0];
+        }
+        
+        for (int j = 1; j < n; j++) {
+            if (obstacleGrid[0][j] != 1) pathCounts[0][j] = pathCounts[0][j-1];
+        }
+        
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] != 1) {
+                    pathCounts[i][j] = pathCounts[i-1][j] + pathCounts[i][j-1];
+                }
+            }
+        }
+        
+        return pathCounts[m-1][n-1];
+    }
+    
     // find a path in a obstacle grid
     public ArrayList<Point> getPath(int x, int y) {
         ArrayList<Point> path = new ArrayList<Point>();
