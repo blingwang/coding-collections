6c1a8c42e3bff82d44ffa557795410d69d049b7f
Edward <blingwang@gmail.com>
2013-08-24 23:15:32 -0500

    Update Solution8Q10.java

--- cc150/Solution8Q10.java
+++ cc150/Solution8Q10.java
@@ -1,72 +1,68 @@
-import java.util.*;
-class Solution8Q10 {}
-class HashTable<K, V> {
-    private final int MAX_SIZE = 10;
-    LinkedList<Cell<K, V>>[] items;
-
-    public HashTable() {
-        items = (LinkedList<Cell<K, V>>[]) new LinkedList[MAX_SIZE];
-    }
-
-    // Really, really stupid hash
-    public int hashCodeOfKey(K key) {
-        return key.toString().length() % items.length;
-    }
-
-    public void put(K key, V value) {
-        int x = hashCodeOfKey(key);
-        if (items[x] == null) {
-            items[x] = new LinkedList<Cell<K, V>>();
-        }
-
-        LinkedList<Cell<K, V>> collided = items[x];
-
-        // Look for items with same key and replace if found
-        for (Cell<K, V> c : collided) {
-            if (c.equivalent(key)) {
-                collided.remove(c);
-                break;
-            }
-        }
-
-        Cell<K, V> cell = new Cell<K, V>(key, value);
-        collided.add(cell);
-    }
-
-    public V get(K key) {
-        int x = hashCodeOfKey(key);
-        if (items[x] == null) {
-            return null;
-        }
-
-        LinkedList<Cell<K, V>> collided = items[x];
-        for (Cell<K, V> c : collided) {
-            if (c.equivalent(key)) {
-                return c.getValue();
-            }
-        }
-
-        return null;
-    }
-
-    private class Cell<K, V> {
-        private K key;
-        private V value;
-        public Cell(K k, V v) {
-            key = k;
-            value = v;
-        }
-
-        boolean equivalent(Cell<K, V> c) {
-            return equivalent(c.getKey());
-        }
-
-        boolean equivalent(K k) {
-            return key.equals(k);
-        }
-
-        K getKey() { return key; }
-        V getValue() { return value;}
+public class Solution8Q10 { 
+    public static void main(String[] args) {
+        HashTable<Integer, Character> map = new HashTable<Integer, Character>();
+        map.put(1, 'a');
+        map.put(2, 'b');
+        map.put(3, 'c');
+        
+        System.out.println(map.get(2));
     }
 }
-   
+
+class HashTable<K, V> {
+    private static final int INIT_CAPACITY = 37;
+    private int N; // # of entries
+    private int M; // hash table size;
+    private Entry<K, V>[] table; // array of key-value pairs
+    
+    public HashTable() {
+        this(INIT_CAPACITY);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public HashTable(int M) {
+        this.M = M;
+        table = (Entry<K, V>[])new Entry[M];
+    }
+    
+    private int hash(K key) {
+        return (key.hashCode() & 0xfffffff) % M;
+    }
+    
+    public V get(K key) {
+        for (Entry<K, V> e = table[hash(key)]; e != null; e = e.next) {
+            if (e.key.equals(key)) {
+                return e.val;
+            }
+        }
+        return null;
+    }
+    
+    public void put(K key, V val) {
+        if (val == null) throw new IllegalArgumentException();
+        
+        Entry<K, V> first = table[hash(key)];
+        for (Entry<K, V> e = first; e != null; e = e.next) {
+            if (e.key.equals(key)) {
+                e.val = val;
+                return;
+            }
+        }
+        
+        table[hash(key)] = new Entry<K, V>(key, val, first);
+        N++;
+    }
+    
+    // a helper linked list data type
+    private static class Entry<K, V> {
+        private K key;
+        private V val;
+        private Entry<K, V> next;
+
+        public Entry(K key, V val, Entry<K, V> next)  {
+            this.key  = key;
+            this.val  = val;
+            this.next = next;
+        }
+    }
+}
