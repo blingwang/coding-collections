8d10f4f7fb4fd942edef676bc341e224d8ec6b5e
Edward <blingwang@gmail.com>
2013-08-25 20:58:09 -0500

    Update Solution9Q5.java

--- cc150/Solution9Q5.java
+++ cc150/Solution9Q5.java
@@ -1,5 +1,40 @@
 import java.util.*;
 class Solution9Q5 {
+    public static ArrayList<String> permute(String s) {
+        ArrayList<String> result = new ArrayList<String>();
+        enumerate(s.toCharArray(), 0, result);
+        return result;
+    }
+    
+    private static void enumerate(char[] a, int k, ArrayList<String> result) {
+        if (k == a.length) {
+            result.add(new String(a));
+            return;
+        }
+        for (int i = k; i < a.length; i++) {
+            swap(a, k, i);
+            enumerate(a, k+1, result);
+            swap(a, k, i);
+         }
+    }  
+    
+    private static void swap(char[] a, int i, int j) {
+        char temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "abc";
+        
+        for (String perm : permute(s)) {
+            System.out.println(perm);
+        }
+        
+        
+        System.out.println();
+    }
+    
     public static ArrayList<String> getPerms(String str) {
         if (str == null) {
             return null;
