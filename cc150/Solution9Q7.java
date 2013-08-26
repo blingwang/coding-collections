d18dbe9b189efab9ae0ef9741e183f543ea4771b
Edward <blingwang@gmail.com>
2013-08-25 21:34:20 -0500

    Update Solution9Q7.java

--- cc150/Solution9Q7.java
+++ cc150/Solution9Q7.java
@@ -3,29 +3,28 @@ class Solution9Q7 {
         Black, White, Red, Yellow, Green
     }
 
-    boolean paintFill(Color[][] screen, int x, int y, Color ocolor, color ncolor) {
-        if (x < 0 || x >= screen[0].length ||
-            y < 0 || y >= screen.length) {
+    boolean paintFill(Color[][] screen, int x, int y, Color oldColor, Color newColor) {
+        if (x < 0 || x >= screen[0].length || y < 0 || y >= screen.length) {
             return false;
         }
 
-        if (screen[y][x] == ocolor) {
-            screen[y][x] = ncolor;
-            paintFill(screen, x - 1, y, ocolor, ncolor);
-            paintFill(screen, x + 1, y, ocolor, ncolor);
-            paintFill(screen, x, y - 1, ocolor, ncolor);
-            paintFill(screen, x, y + 1, ocolor, ncolor);
+        if (screen[y][x] == oldColor) {
+            screen[y][x] = newColor;
+            paintFill(screen, x - 1, y, oldColor, newColor);
+            paintFill(screen, x + 1, y, oldColor, newColor);
+            paintFill(screen, x, y - 1, oldColor, newColor);
+            paintFill(screen, x, y + 1, oldColor, newColor);
         }
 
         return true;
     }
 
-    boolean paintFill(Color[][] screen, int x, int y, Color ncolor) {
-        if (sreen[y][x] == ncolor) {
+    boolean paintFill(Color[][] screen, int x, int y, Color newColor) {
+        if (screen[y][x] == newColor) {
             return false;
         }
 
-        return paintFill(screen, int x, int y, screen[y][x], ncolor);
+        return paintFill(screen, x, y, screen[y][x], newColor);
     }
 }
 
