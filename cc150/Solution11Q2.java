9b16325ab495817cd37a310ed614b373b2a6dca6
Edward <blingwang@gmail.com>
2013-08-25 23:50:52 -0500

    Update Solution11Q2.java

--- cc150/Solution11Q2.java
+++ cc150/Solution11Q2.java
@@ -1,24 +1,24 @@
 import java.util.*;
 class Solution11Q2 {
     public void groupAnagrams(String[] array) {
-        HashMap<String, LinkedList<String>> hmap = new 
-            HashMap<String, LinkedList<String>>();
+        HashMap<String, ArrayList<String>> hmap = new 
+            HashMap<String, ArrayList<String>>();
 
-        // Group words by anagram
+        // Group words by anagram into hashtable
         for (String word : array) {
             String key = sortChars(word);
             if (!hmap.containsKey(key)) {
-                hmap.put(key, new LinkedList<String>());
+                hmap.put(key, new ArrayList<String>());
             }
             
-            LinkedList<String> anagrams = hmap.get(key);
-            anagrams.push(word);
+            ArrayList<String> anagrams = hmap.get(key);
+            anagrams.add(word);
         }
 
         // convert hash table to array
         int index = 0;
         for (String key : hmap.keySet()) {
-            LinkedList<String> list = hmap.get(key);
+            ArrayList<String> list = hmap.get(key);
             for (String word : list) {
                 array[index] = word;
                 index++;
