4bc950198a9a0c446a3bd8ec9ca71c262bc5ce07
Edward <blingwang@gmail.com>
2013-10-03 01:04:55 -0500

    Update BinaryTree.java

--- white-board/BinaryTree.java
+++ white-board/BinaryTree.java
@@ -1,70 +1,70 @@
 import java.util.*;
 public class BinaryTree implements Iterable<TreeNode>{
-        private TreeNode root;
+    private TreeNode root;
 
-        public BinaryTree(TreeNode root) {
-                this.root = root;
+    public BinaryTree(TreeNode root) {
+        this.root = root;
+    }
+    
+    @Override
+    public Iterator<TreeNode> iterator() {
+        return new InorderIterator(root);
+    }
+    
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        BinaryTree bt = new BinaryTree(root);
+        Iterator<TreeNode> it = bt.iterator();
+        
+        while (it.hasNext()) {
+            System.out.println(it.next().val);
+        }
+    }
+    
+    private class InorderIterator implements Iterator<TreeNode> {
+        private TreeNode current;
+        private ArrayDeque<TreeNode> stack;
+        
+        public InorderIterator(TreeNode root) {
+            current = root;
+            stack = new ArrayDeque<TreeNode>();
+        }
+        
+        @Override 
+        public boolean hasNext() {
+            return current != null || !stack.isEmpty();
         }
         
         @Override
-        public Iterator<TreeNode> iterator() {
-                return new InorderIterator(root);
+        public TreeNode next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            
+            TreeNode next = stack.pop();
+            current = next.right;
+            
+            return next;
         }
         
-        public static void main(String[] args) {
-                TreeNode root = new TreeNode(1);
-                root.left = new TreeNode(2);
-                root.right = new TreeNode(3);
-                BinaryTree bt = new BinaryTree(root);
-                Iterator<TreeNode> it = bt.iterator();
-                
-                while (it.hasNext()) {
-                        System.out.println(it.next().val);
-                }
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
         }
-        
-        private class InorderIterator implements Iterator<TreeNode> {
-                private TreeNode current;
-                private ArrayDeque<TreeNode> stack;
-                
-                public InorderIterator(TreeNode root) {
-                        current = root;
-                        stack = new ArrayDeque<TreeNode>();
-                }
-                
-                @Override 
-                public boolean hasNext() {
-                        return current != null || !stack.isEmpty();
-                }
-                
-                @Override
-                public TreeNode next() {
-                        if (!hasNext()) throw new NoSuchElementException();
-                        
-                        while (current != null) {
-                                stack.push(current);
-                                current = current.left;
-                        }
-                        
-                        TreeNode next = stack.pop();
-                        current = next.right;
-                        
-                        return next;
-                }
-                
-                @Override
-                public void remove() {
-                        throw new UnsupportedOperationException();
-                }
-        }
+    }
 }
 
 class TreeNode {
-        TreeNode left;
-        TreeNode right;
-        int val;
-        
-        public TreeNode(int val) {
-                this.val = val;
-        }
+    TreeNode left;
+    TreeNode right;
+    int val;
+    
+    public TreeNode(int val) {
+        this.val = val;
+    }
 }
