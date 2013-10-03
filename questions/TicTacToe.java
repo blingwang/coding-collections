5f547c0ff633202c41ca7d7a7ccaa5a3d199c979
Edward <blingwang@gmail.com>
2013-10-03 01:17:12 -0500

    Update TicTacToe.java

--- questions/TicTacToe.java
+++ questions/TicTacToe.java
@@ -1,13 +1,13 @@
 public class TicTacToe{
 enum State{Blank, X, O};
 
-private int n = 3;
+private final int n = 3;
 private State[][] board = new State[n][n];
 private int moveCount;
 
 public void Move(int x, int y, State s){
     if(board[x][y] == State.Blank){
-            board[x][y] = s;
+        board[x][y] = s;
     }
 
     moveCount++;
@@ -16,48 +16,44 @@ public void Move(int x, int y, State s){
 
     //check col
     for(int i = 0; i < n; i++){
-            if(board[x][i] != s)
-                    break;
-            if(i == n-1){
-                    //report win for s
-            }
+        if(board[x][i] != s) break;
+        if(i == n-1){
+            //report win for s
+        }
     }
 
     //check row
     for(int i = 0; i < n; i++){
-            if(board[i][y] != s)
-                    break;
-            if(i == n-1){
-                    //report win for s
-            }
+        if(board[i][y] != s) break;
+        if(i == n-1){
+            //report win for s
+        }
     }
 
     //check diag
     if(x == y){
-            //we're on a diagonal
-            for(int i = 0; i < n; i++){
-                    if(board[i][i] != s)
-                            break;
-                    if(i == n-1){
-                            //report win for s
-                    }
+        //we're on a diagonal
+        for(int i = 0; i < n; i++){
+            if(board[i][i] != s) break;
+            if(i == n-1){
+                //report win for s
             }
+        }
     }
 
     //check anti diag
     if(x + y == n-1){
     for(int i = 0;i<n;i++){
-            if(board[i][(n-1)-i] != s)
-                    break;
-            if(i == n-1){
-                    //report win for s
-            }
+        if(board[i][(n-1)-i] != s) break;
+        if(i == n-1){
+            //report win for s
+        }
     }
     }
 
     //check draw
     if(moveCount == (n^2 - 1)){
-            //report draw
+        //report draw
     }
 }
 }
