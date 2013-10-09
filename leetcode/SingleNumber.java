6e061be0ebb7055b78a6505c90edfa8f7a480fb6
Edward <blingwang@gmail.com>
2013-10-08 22:28:39 -0500

    Create SingleNumber.java

---
+++ leetcode/SingleNumber.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int singleNumber(int[] A) {
+        int result = 0;
+        for (int i = 0; i < A.length; i++) {
+            result ^= A[i];
+        }
+        
+        return result;
+    }
+}
