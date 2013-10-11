fac4f8a84100aebdae7dd3e3ac71e0d36855b078
Edward <blingwang@gmail.com>
2013-10-10 23:59:07 -0500

    Update ThreeSumClosest.java

--- leetcode/ThreeSumClosest.java
+++ leetcode/ThreeSumClosest.java
@@ -2,20 +2,26 @@ import java.util.*;
 class ThreeSumClosest {
     public int threeSumClosest(int[] num, int target) {
         assert(num.length >= 3);
-        int closestSum = num[0] + num[1] + num[2];
         Arrays.sort(num);
+        int closestSum = num[0] + num[1] + num[2];
+        
         for (int i = 0; i < num.length - 2; i++) {
             if (i > 0 && num[i] == num[i-1]) continue;
+            
             int start = i + 1; 
             int end = num.length - 1;
+            
             while (start < end) {
                 int sum = num[i] + num[start] + num[end];
+                
                 if (sum == target) {
                     return target;
                 }
+                
                 if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
                     closestSum = sum;
                 }
+                
                 if (sum < target) {
                     start++;
                 } else {
@@ -23,6 +29,7 @@ class ThreeSumClosest {
                 }
             }
         }
+        
         return closestSum;
     }
 }
