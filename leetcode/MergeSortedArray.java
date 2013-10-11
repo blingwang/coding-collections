32711daa31321e4bd887468b549d0be42ada5ff3
Edward <blingwang@gmail.com>
2013-10-11 01:29:36 -0500

    Update MergeSortedArray.java

--- leetcode/MergeSortedArray.java
+++ leetcode/MergeSortedArray.java
@@ -1,7 +1,9 @@
 public class MergeSortedArray {
     public void merge(int A[], int m, int B[], int n) {
+        assert(A.length >= m + n);
         int i = m - 1;
         int j = n - 1;
+        
         for (int k = m+n-1; k >= 0; k--) {
             if (i < 0) A[k] = B[j--];
             else if (j < 0) break;
