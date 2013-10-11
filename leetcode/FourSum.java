e5aa9d1d4658091f7491e13d8c99329523cffa28
Edward <blingwang@gmail.com>
2013-10-11 00:50:24 -0500

    Update FourSum.java

--- leetcode/FourSum.java
+++ leetcode/FourSum.java
@@ -3,16 +3,20 @@ class FourSum {
     public ArrayList<ArrayList<Integer>> fourSum(int[] num, int target) {
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
         Arrays.sort(num);
+        
         for (int i = 0; i < num.length - 3; i++) {
             if ( i > 0 && num[i] == num[i-1]) continue; 
+            
             for (int j = i + 1; j < num.length - 2; j++) {
                 if (j > i + 1 && num[j] == num[j-1]) continue;
+                
                 // Do two sum
                 int expected = target-(num[i] + num[j]);
                 int start = j + 1; 
                 int end = num.length - 1;
                 while (start < end) {
                     int sum = num[start] + num[end];
+                    
                     if (sum < expected) {
                         start++;
                     } else if (sum > expected) {
@@ -21,14 +25,17 @@ class FourSum {
                         int curStart = num[start];
                         int curEnd = num[end];
                         ArrayList<Integer> quadruplet = new ArrayList<Integer>(4);
+                        
                         quadruplet.add(num[i]);
                         quadruplet.add(num[j]);
                         quadruplet.add(curStart);
                         quadruplet.add(curEnd);
                         result.add(quadruplet);
+                        
                         while(start < end &&  num[start] == curStart) {
                             start++;
                         }
+                        
                         while(end > start && num[end] == curEnd) {
                             end--;
                         }
@@ -36,6 +43,7 @@ class FourSum {
                 }
             }     
         }
+        
         return result;       
     }
 }
