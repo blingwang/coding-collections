853d69ff5d706d04e2f7103753a4ccef27541617
Edward <blingwang@gmail.com>
2013-10-12 00:57:54 -0500

    Update InsertIntervals.java

--- leetcode/InsertIntervals.java
+++ leetcode/InsertIntervals.java
@@ -1,28 +1,29 @@
 import java.util.*;
 public class InsertIntervals {
    public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
-       ArrayList<Interval> mergedList = new ArrayList<Interval>();
-       
-       int i = 0;
-       // if origion data structure can be efficently inserted, use binary search
-       while (i < intervals.size() && newInterval.start > intervals.get(i).end) {
-           mergedList.add(intervals.get(i++));
-       }
-       
-       while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
-           newInterval.end = Math.max(newInterval.end, intervals.get(i).end);
-           newInterval.start = Math.min(newInterval.start, intervals.get(i).start);
-           i++;
-       }
-       
-       mergedList.add(newInterval);
-       
-       while(i < intervals.size()) {
-           mergedList.add(intervals.get(i++));
-       }
-       
-       return mergedList;
-   }
+        ArrayList<Interval> mergedList = new ArrayList<Interval>();
+        int i = 0;
+        
+        // if origion data structure can be efficently inserted, use binary search
+        while (i < intervals.size() && intervals.get(i).end < newInterval.start) {
+            mergedList.add(intervals.get(i));
+            i++;
+        }
+        
+        while (i < intervals.size() && intervals.get(i).start <= newInterval.end) {
+            newInterval.start = Math.min(intervals.get(i).start, newInterval.start);
+            newInterval.end = Math.max(intervals.get(i).end, newInterval.end);
+            i++;
+        }
+        mergedList.add(newInterval);
+        
+        while (i < intervals.size()) {
+            mergedList.add(intervals.get(i));
+            i++;
+        }
+        
+        return mergedList;
+    }
    
    public ArrayList<Interval> insertInplace(ArrayList<Interval> intervals, Interval newInterval) {
         int insertIndex = findInsertPosition(intervals, newInterval);
