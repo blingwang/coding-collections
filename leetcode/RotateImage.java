207de184e10c88c6767a2503fd65546fe447efad
Edward <blingwang@gmail.com>
2013-10-12 13:00:48 -0500

    Update RotateImage.java

--- leetcode/RotateImage.java
+++ leetcode/RotateImage.java
@@ -27,13 +27,13 @@ public class RotateImage {
         int n = matrix.length;
         assert(n > 0 && n == matrix[0].length);
         
-        for (int layer = 0; layer < n/2; layer++) { // skip single core element
-            for (int i = layer; i < n - 1 - layer; i++) {
-                int temp = matrix[layer][i];
-                matrix[layer][i] = matrix[n-1-i][layer];
-                matrix[n-1-i][layer] = matrix[n-1-layer][n-1-i];
-                matrix[n-1-layer][n-1-i] = matrix[i][n-1-layer];
-                matrix[i][n-1-layer] = temp;
+        for (int i = 0; i < n / 2; i++) { // i=layer, skip single core for odd n
+            for (int j =i; j < n - 1 - i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = temp;
             }
         }
     }
