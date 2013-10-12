9de21859dc0dd72277b4c575d52403cb6b61918d
Edward <blingwang@gmail.com>
2013-10-12 12:34:44 -0500

    Update RotateImage.java

--- leetcode/RotateImage.java
+++ leetcode/RotateImage.java
@@ -4,36 +4,36 @@ public class RotateImage {
     }
     public void rotateByTranspose(int[][] matrix) {
         int n = matrix.length;
-        assert(n == matrix[0].length);
+        assert(n > 0 && n == matrix[0].length);
         
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n-i-1; j++) {// swap half elements
+            for (int j = 0; j < n-1-i; j++) {// swap half elements
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[n-j-1][n-i-1];
-                matrix[n-j-1][n-i-1] = temp;
+                matrix[i][j] = matrix[n-1-j][n-1-i];
+                matrix[n-1-j][n-1-i] = temp;
             }
         }
         
         for (int j = 0; j < n; j++) {
             for (int i = 0; i < n/2; i++) {//swap n/2 elements
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[n-i-1][j];
-                matrix[n-i-1][j] = temp;
+                matrix[i][j] = matrix[n-1-i][j];
+                matrix[n-1-i][j] = temp;
             }
         }
     }
 
     public void rotateByLayer(int[][] matrix) {
         int n = matrix.length;
-        assert(n == matrix[0].length);
+        assert(n > 0 && n == matrix[0].length);
         
-        for (int layer = 0; layer < n/2; layer++) {
-            for (int i = layer; i < n - layer - 1; i++) {
+        for (int layer = 0; layer < n/2; layer++) { // skip single core element
+            for (int i = layer; i < n - 1 - layer; i++) {
                 int temp = matrix[layer][i];
-                matrix[layer][i] = matrix[n-i-1][layer];
-                matrix[n-i-1][layer] = matrix[n-layer-1][n-i-1];
-                matrix[n-layer-1][n-i-1] = matrix[i][n-layer-1];
-                matrix[i][n-layer-1] = temp;
+                matrix[layer][i] = matrix[n-1-i][layer];
+                matrix[n-1-i][layer] = matrix[n-1-layer][n-1-i];
+                matrix[n-1-layer][n-1-i] = matrix[i][n-1-layer];
+                matrix[i][n-1-layer] = temp;
             }
         }
     }
