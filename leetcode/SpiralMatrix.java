de681f1987b57aac7bcb0b68284caf4cdacb42ae
Edward <blingwang@gmail.com>
2013-10-13 10:53:07 -0500

    Update SpiralMatrix.java

--- leetcode/SpiralMatrix.java
+++ leetcode/SpiralMatrix.java
@@ -97,4 +97,51 @@ public class SpiralMatrix {
         
         return result;
     }
+    
+    public ArrayList<Integer> spiralOrderIter2(int[][] matrix) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        int m = matrix.length;
+        if (m == 0) return result;
+        int n = matrix[0].length;
+        if (n == 0) return result;
+        
+        int layer = 0, rows = m, cols = n; 
+        while (rows > 0 && cols > 0) {
+            if (rows == 1) {
+                for (int j = 0; j < cols; j++) {
+                    result.add(matrix[layer][layer+j]);
+                }
+                return result;
+            }
+            
+            if (cols == 1) {
+                for (int i = 0; i < rows; i++) {
+                    result.add(matrix[layer+i][layer]);
+                }
+                return result;
+            }
+            
+            for (int j = 0; j < cols - 1; j++) {
+                result.add(matrix[layer][layer+j]);
+            }
+            
+            for (int i = 0; i < rows - 1; i++) {
+                result.add(matrix[layer+i][n-1-layer]);
+            }
+            
+            for (int j = cols - 1; j > 0; j--) {
+                result.add(matrix[m-1-layer][layer+j]);
+            }
+            
+            for (int i = rows - 1; i > 0; i--) {
+                result.add(matrix[layer+i][layer]);
+            }
+            
+            layer++;
+            rows -= 2;
+            cols -= 2;
+        }
+        
+        return result;
+    }
 }
