6ca432f4d1a4d388bec4338cd92f8f7ecff634e2
Edward <blingwang@gmail.com>
2013-10-13 11:42:08 -0500

    Update SpiralMatrix2.java

--- leetcode/SpiralMatrix2.java
+++ leetcode/SpiralMatrix2.java
@@ -2,23 +2,23 @@ public class SpiralMatrix2 {
     public int[][] generateMatrix(int n) {
         if (n <= 0) return new int[0][0];
         
-        int[][] matrix= new int[n][n];
+        int[][] matrix = new int[n][n];
         int curNum = 1;
         
-        for(int layer = 0; (layer+1) * 2 <= n; layer++) {
-            for (int j = layer; j < n-layer-1; j++) {
+        for (int layer = 0; layer < n / 2; layer++) {
+            for (int j = layer; j < n - 1 - layer; j++) {
                 matrix[layer][j] = curNum++;
             }
             
-            for (int i = layer; i < n-layer-1; i++) {
-                matrix[i][n-layer-1] = curNum++;
+            for (int i = layer; i < n - 1 - layer; i++) {
+                matrix[i][n-1-layer] = curNum++;
             }
             
-            for (int j = n-layer-1; j > layer; j--) {
-                matrix[n-layer-1][j] = curNum++;
+            for (int j = n - 1 - layer; j > layer; j--) {
+                matrix[n-1-layer][j] = curNum++;
             }
             
-            for (int i = n-layer-1; i > layer; i--) {
+            for (int i = n - 1 - layer; i > layer; i--) {
                 matrix[i][layer] = curNum++;
             }
         }
