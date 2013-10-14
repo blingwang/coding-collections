9e54b78ad68d93b0320fe16a1f9c29162d1ada14
Edward <blingwang@gmail.com>
2013-10-14 01:11:18 -0500

    Update Search2DMatrix.java

--- leetcode/Search2DMatrix.java
+++ leetcode/Search2DMatrix.java
@@ -51,49 +51,36 @@ class Search2DMatrix {
     private boolean findElementBinPart(int[][] matrix, int target, int u, int l, int d, int r) {
         if (u > d || l > r) return false;
         if (target < matrix[u][l] || target > matrix[d][r]) return false;
-
+        
         // find first element greater than target in middle row
-        int mid = u + (d - u) / 2;
-        int col = findFirstNoLessThan(matrix[mid], target, l, r);
-
+        int midRow = u + (d-u)/2;
+        int col = findFirstEqualOrGreater(matrix[midRow], target, l, r);
+        
         if (col < 0) { // no element equal or greater than target is found
-            return findElementBinPart(matrix, target, mid+1, l, d, r);
+           return findElementBinPart(matrix, target, midRow+1, l, d, r);
         }
-
-        if (target == matrix[mid][col]) return true;
-
-        return findElementBinPart(matrix, target, mid+1, l, d, col-1) || 
-               findElementBinPart(matrix, target, u, col, mid-1, r);
+        
+        if (matrix[midRow][col] == target) {
+            return true;
+        }
+        
+        return findElementBinPart(matrix, target, u, col, midRow-1, r) ||
+               findElementBinPart(matrix, target, midRow+1, l, d, col-1);
     }
-
-    private int findFirstNoLessThan(int[] a, int value, int start, int end) {
-        if (start > end) return -1;
-
-        int left = start;
-        int right = end;
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (value == a[mid]) return mid;
-            if (value < a[mid]) {
+    
+    private int findFirstEqualOrGreater(int[] a, int target, int left, int right) {
+        int bestSoFar = -1;
+        while (left <= right) {
+            int mid = left + (right - left);
+            if (a[mid] == target) {
+                return mid;
+            } else if (a[mid] > target) {
+                bestSoFar = mid;
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-
-        // Now only one element left: left==right
-        if (value <= a[left]) return left;
-        return left+1 > end ? -1 : left+1;
-    }   
-
-    private int findFirstNoLessThanR(int[] a, int value, int start, int end) {
-        if(start > end) return -1;
-        int mid = start + (end - start) / 2;
-        if (value == a[mid]) return mid;
-        if (value > a[mid]) return findFirstNoLessThanR(a, value, mid+1, end);
-        int found = findFirstNoLessThanR(a, value, start, mid-1);
-        if(found < 0) return mid;
-        return found;
+        return bestSoFar;
     }
 }
