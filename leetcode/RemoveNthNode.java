a126897129a8f273c4d5168a899083b6ad67f691
Edward <blingwang@gmail.com>
2013-10-15 00:21:25 -0500

    Update RemoveNthNode.java

--- leetcode/RemoveNthNode.java
+++ leetcode/RemoveNthNode.java
@@ -1,5 +1,27 @@
 public class RemoveNthNode {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        assert(n >= 1);
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        for (int i = 0; i < n; i++) {
+            if (fast == null) return head; // < n nodes
+            fast = fast.next;
+        }
+        
+        if (fast == null) return head.next; // = n nodes
+        
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        slow.next = slow.next.next;
+        
+        return head;
+    }
+    
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
         assert(n > 0);
         if (head == null) return null;
         
