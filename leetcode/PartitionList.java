06b8cbaea5e21906a1a0bffb7d1b3d12bda9701b
Edward <blingwang@gmail.com>
2013-10-15 00:47:23 -0500

    Update PartitionList.java

--- leetcode/PartitionList.java
+++ leetcode/PartitionList.java
@@ -1,26 +1,24 @@
 public class PartitionList {
     public ListNode partition(ListNode head, int x) {
-        ListNode dummyHead = new ListNode(-1);
-        ListNode pivot = new ListNode(-1);
-        ListNode lt = dummyHead;
-        ListNode ge = pivot;
-        ListNode current = head;
+        ListNode lessHead = new ListNode(0);
+        ListNode greaterHead = new ListNode(0);
+        ListNode less = lessHead;
+        ListNode greater = greaterHead;
         
-        while (current != null) {
-            if (current.val < x) {
-                lt.next = current;
-                lt = lt.next;
+        for (ListNode cur = head; cur != null; cur = cur.next) {
+            if (cur.val < x) {
+                less.next = cur;
+                less = less.next;
             } else {
-                ge.next = current;
-                ge = ge.next;
+                greater.next = cur;
+                greater = greater.next;
             }
-            current = current.next;
         }
         
-        lt.next = pivot.next;
-        ge.next = null;
+        less.next = greaterHead.next;
+        greater.next = null;
         
-        return dummyHead.next;
+        return lessHead.next;
     }
 
     private class ListNode {
