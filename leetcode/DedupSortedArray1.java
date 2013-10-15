5f3fd729f15418000c3f70fba3a2d85d7568a596
Edward <blingwang@gmail.com>
2013-10-14 22:57:56 -0500

    Update DedupSortedArray1.java

--- leetcode/DedupSortedArray1.java
+++ leetcode/DedupSortedArray1.java
@@ -1,15 +1,17 @@
 public class DedupSortedArray1 {
     public int removeDuplicates(int[] A) {
+        public int removeDuplicates(int[] A) {
         if (A.length == 0) return 0;
-        int updateIndex = 1;
-        int pre = A[0];
         
+        int updateIndex = 1;
         for (int i = 1; i < A.length; i++) {
-            if (A[i] != pre) {
-               A[updateIndex++] = A[i];
-               pre = A[i];
+            if (A[i] != A[i-1]) {
+                A[updateIndex] = A[i];
+                updateIndex++;
             }
         }
+        
         return updateIndex;
+    }
     }
 }
