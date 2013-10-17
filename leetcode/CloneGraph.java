a7c87517c9520d8fcad54d7ef4766ac9f626bdf8
Edward <blingwang@gmail.com>
2013-10-17 01:05:06 -0500

    Update CloneGraph.java

--- leetcode/CloneGraph.java
+++ leetcode/CloneGraph.java
@@ -7,8 +7,37 @@ class UndirectedGraphNode {
     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
 }
 
-public class Solution {
+public class CloneGraph {
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        if (node == null) return null;
+        ArrayDeque<UndirectedGraphNode> queue = new ArrayDeque<UndirectedGraphNode>();
+        Map<Integer, UndirectedGraphNode> copyTable = new HashMap<Integer, UndirectedGraphNode>();
+        
+        queue.offer(node);
+        UndirectedGraphNode graphCopy = new UndirectedGraphNode(node.label);
+        copyTable.put(node.label, graphCopy);
+        
+        while (!queue.isEmpty()) {
+            UndirectedGraphNode current = queue.poll();
+            UndirectedGraphNode currentCopy = copyTable.get(current.label);
+            
+            for (UndirectedGraphNode n : current.neighbors) { // visit edge
+                if (!copyTable.containsKey(n.label)) {
+                    UndirectedGraphNode copy = new UndirectedGraphNode(n.label);
+                    currentCopy.neighbors.add(copy);
+                    copyTable.put(n.label, copy);
+                    queue.offer(n);
+                } else {
+                    UndirectedGraphNode copy = copyTable.get(n.label);
+                    currentCopy.neighbors.add(copy);
+                }
+            }
+        }
+        
+        return graphCopy;
+    }
+    
+    public UndirectedGraphNode cloneGraphDFS(UndirectedGraphNode node) {
         Map<Integer, UndirectedGraphNode> copyTable = new HashMap<Integer, UndirectedGraphNode>();
         return clone(node, copyTable);
     }
