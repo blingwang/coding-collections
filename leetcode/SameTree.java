858a82cba8d5118720c3b4106ca30183348d6d64
Edward <blingwang@gmail.com>
2013-10-17 22:26:42 -0500

    Update SameTree.java

--- leetcode/SameTree.java
+++ leetcode/SameTree.java
@@ -5,6 +5,7 @@ public class SameTree {
         TreeNode right;
         TreeNode(int x) { val = x; }
     }
+    
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null && q == null) return true;
         if (p == null || q == null) return false;
