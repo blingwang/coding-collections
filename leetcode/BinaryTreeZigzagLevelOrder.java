0cce833e2fc55f6c5c8185e10eaa0552a43eb8ea
Edward <blingwang@gmail.com>
2013-10-19 11:09:55 -0500

    Update BinaryTreeZigzagLevelOrder.java

--- leetcode/BinaryTreeZigzagLevelOrder.java
+++ leetcode/BinaryTreeZigzagLevelOrder.java
@@ -1,13 +1,14 @@
 import java.util.*;
 public class BinaryTreeZigzagLevelOrder {
-        public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        if (root == null) return result;
+    public ArrayList<ArrayList<Integer>> zigzagLevelOrder(TreeNode root) {
+        ArrayList<ArrayList<Integer>> levelList = new ArrayList<ArrayList<Integer>>();
+        if (root == null) return levelList;
         
         ArrayDeque<TreeNode> currentLevel = new ArrayDeque<TreeNode>();
-        ArrayDeque<TreeNode> nextLevel = new ArrayDeque<TreeNode>();   
-        boolean leftToRight = true;
-        nextLevel.add(root);
+        ArrayDeque<TreeNode> nextLevel = new ArrayDeque<TreeNode>();
+        boolean leftToRight = false; // 2nd level: push left to right, pop right to left
+        
+        nextLevel.offer(root);
         
         while (!currentLevel.isEmpty() || !nextLevel.isEmpty()) {
             if (currentLevel.isEmpty()) {
@@ -15,24 +16,23 @@ public class BinaryTreeZigzagLevelOrder {
                 currentLevel = nextLevel;
                 nextLevel = temp;
                 leftToRight = !leftToRight;
-                result.add(new ArrayList<Integer>());
+                levelList.add(new ArrayList<Integer>());
             }
             
             TreeNode node = currentLevel.pop();
-            ArrayList<Integer> list = result.get(result.size()-1);
-            list.add(node.val);
+            ArrayList<Integer> currentList = levelList.get(levelList.size() - 1);
+            currentList.add(node.val);
             
             if (leftToRight) {
-                if (node.right != null) nextLevel.push(node.right);
                 if (node.left != null) nextLevel.push(node.left);
+                if (node.right != null) nextLevel.push(node.right);
             } else {
-                if (node.left != null) nextLevel.push(node.left);
                 if (node.right != null) nextLevel.push(node.right);
+                if (node.left != null) nextLevel.push(node.left);
             }
-            
         }
         
-        return result;
+        return levelList;
     }
     
     private class TreeNode {
