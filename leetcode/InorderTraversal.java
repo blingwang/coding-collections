6069dbaad8c7612fe8adf808c0eeba7a04ba360e
Edward <blingwang@gmail.com>
2013-10-19 12:04:51 -0500

    Update InorderTraversal.java

--- leetcode/InorderTraversal.java
+++ leetcode/InorderTraversal.java
@@ -1,26 +1,27 @@
 import java.util.*;
 public class InorderTraversal {
    public ArrayList<Integer> inorderTraversal(TreeNode root) {
-       ArrayList<Integer> values = new ArrayList<Integer>();
-       ArrayDeque<TreeNode> stack = new ArrayDeque<TreeNode>();
-       TreeNode current = root;
-       
-       while (true) {
-           while (current != null) {
-               stack.push(current);
-               current = current.left;
-           } 
-           
-           if (stack.isEmpty()) break; // done
-           
-           // always pop when cur is null(left end or right end)
-           current = stack.pop();
-           values.add(current.val);
-           current = current.right;
-       }
-       
-       return values;
-   }
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        ArrayDeque<TreeNode> stack = new ArrayDeque<TreeNode>();
+        TreeNode current = root; // current node to visit
+        
+        while (true) {
+            while (current != null) {// got to left end
+                stack.push(current);
+                current = current.left;
+            }
+            
+            if (stack.isEmpty()) break; // done
+            
+            // visit parent: not to visit left for nodes from stack
+            // always pop when cur is null(left end or right end)
+            current = stack.pop(); 
+            result.add(current.val);
+            current = current.right; // visit right substree
+        }
+        
+        return result;
+    }
    
    public ArrayList<Integer> inorderTraversalRecur(TreeNode root) {
        ArrayList<Integer> values = new ArrayList<Integer>();
