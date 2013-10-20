63d6e47a6c873d04b93df48768d20d50bd95cc19
Edward <blingwang@gmail.com>
2013-10-19 22:10:38 -0500

    Update PathSum.java

--- leetcode/PathSum.java
+++ leetcode/PathSum.java
@@ -1,14 +1,8 @@
 public class PathSum {
     public boolean hasPathSum(TreeNode root, int sum) {
-        return dfs(root, sum);
-    }
-
-    private boolean dfs(TreeNode root, int sum) {
         if (root == null) return false;
-        
-        if (root.left == null && root.right == null && root.val == sum) return true;
-        
-        return dfs(root.left, sum - root.val) || dfs(root.right, sum - root.val);
+        if (root.left == null && root.right == null) return root.val == sum;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 
     private class TreeNode {
