d09fa685684101e312ebeaf0b7269ca22251efe0
Edward <blingwang@gmail.com>
2013-10-21 01:41:50 -0500

    Update PopulateNextRightPointers2.java

--- leetcode/PopulateNextRightPointers2.java
+++ leetcode/PopulateNextRightPointers2.java
@@ -1,20 +1,24 @@
 public class PopulateNextRightPointers2 {
     public void connect(TreeLinkNode root) {
         if (root == null) return;
+        
         TreeLinkNode nextRoot = null;
         TreeLinkNode preNode = null;
+        
         for (TreeLinkNode current = root; current != null; current = current.next) {
             if (current.left != null) {
                 if (nextRoot == null) nextRoot = current.left;
                 if (preNode != null) preNode.next = current.left;
                 preNode = current.left;
             }
+            
             if (current.right != null) {
                 if (nextRoot == null) nextRoot = current.right;
                 if (preNode != null) preNode.next = current.right;
                 preNode = current.right;
             }
         }
+        
         connect(nextRoot);
     }
 
