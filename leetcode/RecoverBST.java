0b9fa2224e55557e328e795f35717286e4e530f5
Edward <blingwang@gmail.com>
2013-10-20 23:11:43 -0500

    Update RecoverBST.java

--- leetcode/RecoverBST.java
+++ leetcode/RecoverBST.java
@@ -1,7 +1,41 @@
 import java.util.*;
 public class RecoverBST {
-    TreeNode first, second, pre;
+    public void recoverTree(TreeNode root) {
+        TreeNode[] swappedPair = new TreeNode[2];
+        inorder(root, swappedPair, null);
+        swapValues(swappedPair[0], swappedPair[1]);
+    }
     
+    // [1, 4, 3, 2, 5]: larger one in first inverted pair and smaller in the second pair
+    // [1, 3, 2, 4, 5]: the only inverted pair is the two elements we are looking for
+    // in-order traversal and return the last visited node in the traversal
+    private TreeNode inorder(TreeNode root, TreeNode[] swappedPair, TreeNode pre) {
+        if (root == null) return pre;
+        pre = inorder(root.left, swappedPair, pre);
+        
+        if (pre != null && root.val < pre.val) {
+            if (swappedPair[0] == null) {
+                swappedPair[0] = pre;
+                swappedPair[1] = root;
+            } else {
+                swappedPair[1] = root;
+                return root;
+            }
+        }
+        
+        pre = root;
+        
+        return inorder(root.right, swappedPair, pre);
+    }
+    
+    private void swapValues(TreeNode p, TreeNode q) {
+        if (p == null || q == null) return;
+        int temp= p.val;
+        p.val = q.val;
+        q.val = temp;
+    }
+    
+    TreeNode first, second, pre;
     public void recoverTree(TreeNode root) {
         first = null;
         second = null;
@@ -26,13 +60,6 @@ public class RecoverBST {
         }
         pre = root;
         inorderSearchPivots(root.right);
-    }
-    
-    private void swapValue(TreeNode a, TreeNode b) {
-        if (first == null || second == null) return;
-        int temp = a.val;
-        a.val = b.val;
-        b.val = temp;
     }
     
     private class TreeNode {
