237aecf80ae79507e63d8dbef6974a8c0579d0f4
Edward <blingwang@gmail.com>
2013-10-22 00:08:46 -0500

    Update DivideIntegers.java

--- leetcode/DivideIntegers.java
+++ leetcode/DivideIntegers.java
@@ -2,20 +2,20 @@ public class DivideIntegers {
     public int divide(int dividend, int divisor) {
         assert(divisor != 0);
         if (dividend == 0 || divisor == 1) return dividend;
-        if (dividend == -1) return -dividend;
+        if (divisor == -1) return -dividend;
         
-        long dvd = (long)Math.abs((double)dividend);
-        long dvs = (long)Math.abs((double)divisor);
-        int ret = 0;
+        long divd = (long)Math.abs((double)dividend);
+        long divs = (long)Math.abs((double)divisor);
+        int result = 0;
         
-        while (dvd >= dvs) {
-            long temp = dvs;
-            for (int i = 0; dvd >= temp; i++, temp <<= 1) {
-                dvd -= temp;
-                ret += 1 << i;
+        while (divd >= divs) {
+            long curDivisor = divs;
+            for (int i = 0; curDivisor <= divd; i++, curDivisor <<= 1) {
+                divd -= curDivisor;
+                result += 1 << i;
             }
         }
         
-        return ((dividend ^ divisor) >> 31) == 0 ? ret : -ret;
+        return ((dividend ^ divisor) >> 31) == 0 ? result : -result;
     }
 }
