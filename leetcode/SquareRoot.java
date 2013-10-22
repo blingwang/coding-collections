41521418377320cd85d8ca20204b2633fe4740c7
Edward <blingwang@gmail.com>
2013-10-22 00:38:44 -0500

    Update SquareRoot.java

--- leetcode/SquareRoot.java
+++ leetcode/SquareRoot.java
@@ -1,19 +1,27 @@
 public class SquareRoot {
     public int sqrt(int x) {
         assert(x >= 0);
-        if (x < 2) return x;
+        if (x < 2) return x; // if x=0, mid=0;
+        return bsearch(x, 1, x);
+    }
+    
+    private int bsearch(int square, int lo, int hi) {
+        int bestSoFar = -1;
         
-        int left = 1, right = x;
-        
-        // find last two that left <= target <= right since no duplicates
-        while (right - left > 1) {
-            int mid = left + (right - left) / 2;
-            if (x / mid == mid) return mid;
-            if (x / mid < mid) right = mid;
-            else left = mid;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (mid == square / mid) { // mid*mid->overflow
+                return mid;
+            } else if (mid < square / mid) {
+                bestSoFar = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
         }
         
-        return left;
+        return bestSoFar;
     }
     
     // Newton's method using relative epsilon to avoid infinite loop
