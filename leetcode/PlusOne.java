b8abcacb206787cb89c753dc2bf3476f9c8ec532
Edward <blingwang@gmail.com>
2013-10-21 21:52:38 -0500

    Update PlusOne.java

--- leetcode/PlusOne.java
+++ leetcode/PlusOne.java
@@ -1,27 +1,37 @@
 public class PlusOne {
     public int[] plusOne(int[] digits) {
-        int[] result = new int[digits.length]; // keep original?
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        
+        int[] result = new int[digits.length+1];
+        result[0] = 1;
+        
+        return result;
+    }
+    
+    public int[] plusOne2(int[] digits) {
         int carrier = 1;
         
         for (int i = digits.length - 1; i >= 0; i--) {
-            if (carrier == 0) {
-                result[i] = digits[i];
-                continue;
-            }
             int sum = digits[i] + carrier;
-            result[i] = sum % 10;
+            
+            if (sum < 10) {
+                digits[i] = sum;
+                return digits;
+            }
+            
+            digits[i] = sum % 10;
             carrier = sum / 10;
         }
         
-        if (carrier != 0) {
-            int[] ret = new int[result.length+1];
-            for (int i = 0; i < result.length; i++) {
-                ret[i+1] = result[i];
-            }
-            
-            ret[0] = carrier;
-            return ret;
-        }
+        int[] result = new int[digits.length+1];
+        result[0] = 1;
         
         return result;
     }
