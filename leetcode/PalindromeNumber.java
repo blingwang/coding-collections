e0af548e4b1cbf1e45cc619cc5a2d7657698d414
Edward <blingwang@gmail.com>
2013-10-24 00:24:38 -0500

    Update PalindromeNumber.java

--- leetcode/PalindromeNumber.java
+++ leetcode/PalindromeNumber.java
@@ -2,7 +2,7 @@ public class PalindromeNumber {
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
         
-        int div = 1;
+        int div = 1; // for single digit
         while (x / div >= 10) {
             div *= 10;
         }
