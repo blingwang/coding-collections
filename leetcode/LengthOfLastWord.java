70e0b2b24b7353034aef110a16128d3e1f40a3a1
Edward <blingwang@gmail.com>
2013-10-25 00:05:54 -0500

    Update LengthOfLastWord.java

--- leetcode/LengthOfLastWord.java
+++ leetcode/LengthOfLastWord.java
@@ -1,19 +1,17 @@
 public class LengthOfLastWord {
     public int lengthOfLastWord(String s) {
-        int count = 0;
+        int length = 0;
         int i = s.length() - 1;
         
-        while (i >= 0) {
-            if (s.charAt(i) != ' ') break;
+        while (i >= 0 && s.charAt(i) == ' ') {
             i--;
         }
         
-        while (i >= 0) {
-            if (s.charAt(i) == ' ') break;
-            count++;
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
             i--;
         }
         
-        return count;
+        return length;
     }
 }
