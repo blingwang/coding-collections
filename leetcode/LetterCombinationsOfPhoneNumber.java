4c72c56d1e46c6b8ae6a19b33c6c3fb5ebba9349
Edward <blingwang@gmail.com>
2013-10-29 00:41:45 -0500

    Update LetterCombinationsOfPhoneNumber.java

--- leetcode/LetterCombinationsOfPhoneNumber.java
+++ leetcode/LetterCombinationsOfPhoneNumber.java
@@ -1,28 +1,30 @@
 import java.util.*;
 public class LetterCombinationsOfPhoneNumber {
-    private static final char[][] map = {{' '}, {}, {'a','b','c'}, {'d','e','f'},
-                                         {'g','h','i'}, {'j','k','l'},
-                                         {'m','n','o'}, {'p','q','r','s'}, 
-                                         {'t','u','v'}, {'w','x','y','z'}};
-    private ArrayList<String> list;
-
+    private static final char[][] charMap = {{' '}, {}, {'a','b','c'}, {'d','e','f'}, 
+                                             {'g','h','i'}, {'j','k','l'}, 
+                                             {'m','n','o'},{'p','q','r','s'}, 
+                                             {'t','u','v'}, {'w','x','y','z'}};
+    private String digits;
+    private ArrayList<String> combinations;
+    
     public ArrayList<String> letterCombinations(String digits) {
-        list = new ArrayList<String>();
+        this.digits = digits;
+        combinations = new ArrayList<String>();
         char[] prefix = new char[digits.length()];
-        enumerate(digits, prefix, 0);
-        return list;
+        enumerate(0, prefix);
+        return combinations;
     }
-
-    private void enumerate(String digits, char[] prefix, int index) {
-        if (index == digits.length()) {
-            list.add(new String(prefix));
+    
+    private void enumerate(int curIndex, char[] prefix) {
+        if (curIndex == digits.length()) {
+            combinations.add(new String(prefix));
             return;
         }
-
-        int num = digits.charAt(index) - '0';
-        for (char c : map[num]) {
-            prefix[index] = c;
-            enumerate(digits, prefix, index+1);
+        
+        int num = digits.charAt(curIndex) - '0';
+        for (char c : charMap[num]) {
+            prefix[curIndex] = c;
+            enumerate(curIndex + 1, prefix);
         }
     }
 }
