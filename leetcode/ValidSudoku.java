353c41b1d66b764330bb47fac0ec2d56eb3ff6c9
Edward <blingwang@gmail.com>
2013-10-30 01:34:28 -0500

    Update ValidSudoku.java

--- leetcode/ValidSudoku.java
+++ leetcode/ValidSudoku.java
@@ -7,7 +7,7 @@ public class ValidSudoku {
     private boolean isValidRow(char[][] board) {
         boolean[] curRow = new boolean[9];
         for (int row = 0; row < 9; row++) {
-            resetArray(curRow);
+            Arrays.set(curRow, false);
             for (int col = 0; col < 9; col++) {
                 char c = board[row][col];
                if (c == '.') continue;
@@ -21,7 +21,7 @@ public class ValidSudoku {
     private boolean isValidCol(char[][] board) {
         boolean[] curCol = new boolean[9];
         for (int col = 0; col < 9; col++) {
-            resetArray(curCol);
+            Arrays.set(curCol, false);
             for (int row = 0; row < 9; row++) {
                 char c = board[row][col];
                 if (c == '.') continue;
@@ -36,7 +36,7 @@ public class ValidSudoku {
         boolean[] curBox = new boolean[9];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                resetArray(curBox);
+                Arrays.set(curBox, false);
                 int startRow = i * 3;
                 int startCol = j * 3;
                 for (int m = 0; m < 3; m++) {
@@ -47,60 +47,6 @@ public class ValidSudoku {
                         curBox[c-'1'] = true;
                     }
                 }
-            }
-        }
-        return true;
-    }
-
-    private void resetArray(boolean[] a) {
-        for (int i = 0; i < a.length; i++) {
-            a[i] = false;
-        }
-    }
-        
-    public boolean isValidSudokuSlow(char[][] board) {
-        assert(board.length == 9 && board[0].length == 9);
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                char c = board[i][j];
-                if (c == '.') continue;
-                if (!isRowValid(board, c, i, j) || !isColValid(board, c, i, j) ||
-                    !isBoxValid(board, c, i, j)) {
-                        return false;
-                    }
-            }
-        }
-        return true;
-    }
-
-    private boolean isRowValid(char[][] board, char num, int row, int col) {
-        for (int i = 0; i < col; i++) {
-            if (board[row][i] == num) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean isColValid(char[][] board, char num, int row, int col) {
-        for (int i = 0; i < row; i++) {
-            if (board[i][col] == num) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean isBoxValid(char[][] board, char num, int row, int col) {
-        int startRow = (row / 3) * 3;
-        int startCol = (col / 3) * 3;
-        
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                int r = startRow + i;
-                int c = startCol + j;
-                if (r == row && c == col) continue; 
-                if (board[r][c] == num) return false;
             }
         }
         return true;
