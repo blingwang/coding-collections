830e1278e447fcb731ed38de182edca14983f8c2
Edward <blingwang@gmail.com>
2013-11-01 00:21:21 -0500

    Update WordSearch.java

--- leetcode/WordSearch.java
+++ leetcode/WordSearch.java
@@ -4,41 +4,40 @@ public class WordSearch {
     private char[][] board;
     private int m, n;
     private String word;
-    private HashSet<Integer> marked;
-
+    private boolean[][] marked;
+    
     public boolean exist(char[][] board, String word) {
-        if (board.length == 0) return false;
+        this.board = board;
         m = board.length;
         n = board[0].length;
-        this.board = board;
         this.word = word;
-        marked = new HashSet<Integer>();
+        marked = new boolean[m][n];
         
-        if(m * n < word.length()) return false;
+        if (m * n < word.length()) return false;
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (dfs(i, j, 0)) return true;
+                if (searchWord(i, j, 0)) return true;
             }
         }
         
         return false;
     }
-
-    private boolean dfs(int i, int j, int count) {
-        if ( count == word.length()) return true;
-        if ( i < 0 || i >= m || j < 0 || j >= n) return false;
-        if (marked.contains(i*n+j)) return false;
-        if ( board[i][j] != word.charAt(count)) return false;
+    
+    private boolean searchWord(int i, int j, int index) {
+        if (index == word.length()) return true;
+        if (i < 0 || i >= m || j < 0 || j >= n) return false;
+        if (marked[i][j]) return false;
+        if (board[i][j] != word.charAt(index)) return false;
         
-        marked.add(i*n+j);
+        marked[i][j] = true;
         
-        if (dfs(i-1, j, count+1)) return true;
-        if (dfs(i+1, j, count+1)) return true;
-        if (dfs(i, j-1, count+1)) return true;
-        if (dfs(i, j+1, count+1)) return true;
+        if (searchWord(i-1, j, index+1)) return true;
+        if (searchWord(i+1, j, index+1)) return true;
+        if (searchWord(i, j-1, index+1)) return true;
+        if (searchWord(i, j+1, index+1)) return true;
         
-        marked.remove(i*n+j);
+        marked[i][j] = false;
         
         return false;
     }
