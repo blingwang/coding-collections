6cf9509effadfa8c7c76b5eb419d5900f908286b
Edward <blingwang@gmail.com>
2013-11-07 11:33:21 -0600

    Update SieveOfErastosthenes.java

--- white-board/SieveOfErastosthenes.java
+++ white-board/SieveOfErastosthenes.java
@@ -1,22 +1,26 @@
 class SieveOfErastosthenes {
     private static int [] generatePrimes(int max) {
         boolean[] isComposite = new boolean[max + 1];
-        for (int i = 2; i * i <= max; i++) {
+        
+        for (int i = 2; i * i <= max; i++) {//sieve primes:2~sqrt(n)
             if (!isComposite [i]) {
-                for (int j = i; i * j <= max; j++) {
+                for (int j = i; i * j <= max; j++) {//candidates=i*j:i^2~max
                     isComposite [i*j] = true;
                 }
             }
         }
+        
         int numPrimes = 0;
         for (int i = 2; i <= max; i++) {
             if (!isComposite [i]) numPrimes++;
         }
+        
         int [] primes = new int [numPrimes];
         int index = 0;
         for (int i = 2; i <= max; i++) {
             if (!isComposite [i]) primes [index++] = i;
         }
+        
         return primes;
     }
 }
