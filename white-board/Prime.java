cd368d11f747ebb5e2bf73aba58a7add4718287f
Edward <blingwang@gmail.com>
2013-11-07 12:19:42 -0600

    Update Prime.java

--- white-board/Prime.java
+++ white-board/Prime.java
@@ -1,5 +1,32 @@
 import java.util.*;
 class Prime {
+    /******Sieve of Eratosthenes*****/
+    public static int [] generatePrimes(int max) {
+        assert(max >= 2);
+        boolean[] isComposite = new boolean[max + 1];
+        
+        for (int i = 2; i * i <= max; i++) {//sieve primes:2~sqrt(max)
+            if (!isComposite [i]) {
+                for (int j = i; i * j <= max; j++) {//candidates=i*j:i^2~max
+                    isComposite [i*j] = true;
+                }
+            }
+        }
+        
+        int numPrimes = 0;
+        for (int i = 2; i <= max; i++) {
+            if (!isComposite [i]) numPrimes++;
+        }
+        
+        int [] primes = new int [numPrimes];
+        int index = 0;
+        for (int i = 2; i <= max; i++) {
+            if (!isComposite [i]) primes [index++] = i;
+        }
+        
+        return primes;
+    }
+    
     // naive method, for more advanced algorithms, see:
     // http://en.wikipedia.org/wiki/Integer_factorization
     public static ArrayList<Integer> findFactors(int n) {
