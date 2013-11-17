9210d42342821327fc20fb5aa42cfd253fbee8f2
Edward <blingwang@gmail.com>
2013-11-17 17:56:36 -0600

    Update UniquePaths2.java

--- leetcode/UniquePaths2.java
+++ leetcode/UniquePaths2.java
@@ -55,6 +55,24 @@ public class UniquePaths2 {
         return pathCounts[m-1][n-1];
     }
     
+    public int uniquePathsWithObstaclesDP3(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        assert(m > 0 && n > 0);
+        
+        int[][] dp = new int[m+1][n+1];
+        dp[1][0] = 1;
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (obstacleGrid[i-1][j-1] == 1) dp[i][j] = 0;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        return dp[m][n];
+    }
+    
     // Solution 3 using DP: store in array
     public int uniquePathsWithObstaclesDP1(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
