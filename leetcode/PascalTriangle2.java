f09d75390c2d48f7c1ddbed842ffc4cd86b4f85f
Edward <blingwang@gmail.com>
2013-11-17 19:14:26 -0600

    Update PascalTriangle2.java

--- leetcode/PascalTriangle2.java
+++ leetcode/PascalTriangle2.java
@@ -1,16 +1,16 @@
 import java.util.*;
 public class PascalTriangle2 {
     public ArrayList<Integer> getRow(int rowIndex) {
-        ArrayList<Integer> result = new ArrayList<Integer>(rowIndex+1);
-        result.add(1);
+        ArrayList<Integer> row = new ArrayList<Integer>();
+        row.add(1);
         
-        for (int row = 1; row <= rowIndex; row++) {
-            result.add(1);
-            for (int i = row-1; i > 0; i--) {
-                result.set(i, result.get(i-1) + result.get(i));
+        for (int i = 1; i <= rowIndex; i++) {
+            row.add(1);
+            for (int j = i - 1; j > 0; j--) {
+                row.set(j, row.get(j) + row.get(j-1));
             }
         }
         
-        return result;
+        return row;
     }
 }
