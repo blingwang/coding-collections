a377c2d1afcc6d5e840f99f081ab9b8409cf6ab2
Edward <blingwang@gmail.com>
2013-11-18 00:49:17 -0600

    Update BuySellStock2.java

--- leetcode/BuySellStock2.java
+++ leetcode/BuySellStock2.java
@@ -1,6 +1,6 @@
 public class BuySellStock2 {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length < 2) return 0;
+        if (prices.length < 2) return 0;
         
         int totalProfit = 0;
         for (int i = 1; i < prices.length; i++) {
