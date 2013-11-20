62e3d1b6b94b87524cea8383b29a6985ed1d365c
Edward <blingwang@gmail.com>
2013-11-20 11:26:41 -0600

    Update MergeTwoSortedLists.java

--- leetcode/MergeTwoSortedLists.java
+++ leetcode/MergeTwoSortedLists.java
@@ -1,5 +1,26 @@
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val > l2.val) {
+                current.next = l2;
+                l2 = l2.next;
+            } else {
+                current.next = l1;
+                l1 = l1.next;
+            }
+            
+            current = current.next;
+        }
+        
+        current.next = (l1 == null) ? l2 : l1;
+        
+        return dummyHead.next;
+    }
+    
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
         
         ListNode dummyHead = new ListNode(-1);
         ListNode pre = dummyHead;
