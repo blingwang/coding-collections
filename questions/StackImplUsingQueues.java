13ea7577f0fe3f2b6af4a18c339f0116436a82c9
Edward <blingwang@gmail.com>
2013-11-21 15:48:01 -0600

    Update StackImplUsingQueues.java

--- questions/StackImplUsingQueues.java
+++ questions/StackImplUsingQueues.java
@@ -1,3 +1,20 @@
+Version A:
+
+    push:
+        enqueue in queue1
+    pop:
+        while size of queue1 is bigger than 1, pipe dequeued items from queue1 into queue2
+        dequeue and return the last item of queue1, then switch the names of queue1 and queue2
+
+Version B:
+
+    push:
+        enqueue in queue2
+        enqueue all items of queue1 in queue2, then switch the names of queue1 and queue2
+    pop:
+        deqeue from queue1
+
+
 import java.util.*;
 
 /**
