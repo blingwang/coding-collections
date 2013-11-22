806309eda828d87356078c1df141febef179f4b9
Edward <blingwang@gmail.com>
2013-11-22 17:34:27 -0600

    Update TernarySearchTree.java

--- questions/TernarySearchTree.java
+++ questions/TernarySearchTree.java
@@ -4,36 +4,36 @@ public class TernarySearchTree {
     private Node root;
 
     private class Node {
-	private int value;
-	private Node left, middle, right;
+        private int value;
+        private Node left, middle, right;
 
-	public Node(int value) {
-	    this.value = value;
-	}
+        public Node(int value) {
+            this.value = value;
+        }
     }
 
     public void insert(int value) {
-	root = insert(root, value);
+        root = insert(root, value);
     }
 
     private Node insert(Node node, int value) {
-	if (node == null) {
-	    return new Node(value);
-	}
+        if (node == null) {
+            return new Node(value);
+        }
 
-	if (value < node.value) {
-	    node.left = insert(node.left, value);
-	} else if (value > node.value) {
-	    node.right = insert(node.right, value);
-	} else {
-	    node.middle = insert(node.middle, value);
-	}
+        if (value < node.value) {
+            node.left = insert(node.left, value);
+        } else if (value > node.value) {
+            node.right = insert(node.right, value);
+        } else {
+            node.middle = insert(node.middle, value);
+        }
 
-	return node;
+        return node;
     }
 
     public void delete(int value) {
-	root = delete(root, value);
+        root = delete(root, value);
     }
 
     /*
@@ -45,121 +45,125 @@ public class TernarySearchTree {
      * the minimum value node in the right substree
      */
     private Node delete(Node node, int value) {
-	if (node == null) {
-	    return null; // no such value
-	}
+        if (node == null) {
+            return null; // no such value
+        }
 
-	if (value < node.value) {
-	    node.left = delete(node.left, value);
-	} else if (value > node.value) {
-	    node.right = delete(node.right, value);
-	} else {
-	    if (node.right == null) {
-		return node.left;
-	    }
+        if (value < node.value) {
+            node.left = delete(node.left, value);
+        } else if (value > node.value) {
+            node.right = delete(node.right, value);
+        } else {
+            if (node.right == null) {
+                return node.left;
+            }
 
-	    if (node.left == null) {
-		return node.right;
-	    }
+            if (node.left == null) {
+                return node.right;
+            }
 
-	    Node oldNode = node;
-	    node = min(oldNode.right);
-	    node.right = deleteMin(oldNode.right);
-	    node.left = oldNode.left;
-	}
+            Node oldNode = node;
+            node = min(oldNode.right);
+            node.right = deleteMin(oldNode.right);
+            node.left = oldNode.left;
+        }
 
-	return node;
+        return node;
     }
 
     private Node min(Node node) {
-	if (node == null) {
-	    return null;
-	}
+        if (node == null) {
+            return null;
+        }
 
-	if (node.left == null) {
-	    return node;
-	}
+        if (node.left == null) {
+            return node;
+        }
 
-	return min(node.left);
+        return min(node.left);
     }
 
     private Node deleteMin(Node node) {
-	if (node == null) {
-	    return null;
-	}
+        if (node == null) {
+            return null;
+        }
 
-	if (node.left == null) {
-	    return node.right;
-	}
+        if (node.left == null) {
+            return node.right;
+        }
 
-	node.left = deleteMin(node.left);
-	return node;
+        node.left = deleteMin(node.left);
+        return node;
     }
 
     public void printLevelOrder() {
-	if (root == null) {
-	    return;
-	}
+        if (root == null) {
+            return;
+        }
 
-	ArrayDeque<Node> queue = new ArrayDeque<Node>();
-	int currentCount = 0;
-	int nextCount = 0;
+        ArrayDeque<Node> queue = new ArrayDeque<Node>();
+        int currentCount = 0;
+        int nextCount = 0;
 
-	queue.offer(root);
-	currentCount++;
+        queue.offer(root);
+        currentCount++;
 
-	while (!queue.isEmpty()) {
-	    Node current = queue.poll();
-	    currentCount--;
-	    System.out.print(current.value);
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+            currentCount--;
+            System.out.print(current.value + " ");
 
-	    if (current.left != null) {
-		queue.offer(current.left);
-		nextCount++;
-	    }
+            if (current.left != null) {
+                queue.offer(current.left);
+                nextCount++;
+            }
 
-	    if (current.middle != null) {
-		queue.offer(current.middle);
-		nextCount++;
-	    }
+            if (current.middle != null) {
+                queue.offer(current.middle);
+                nextCount++;
+            }
 
-	    if (current.right != null) {
-		queue.offer(current.right);
-		nextCount++;
-	    }
+            if (current.right != null) {
+                queue.offer(current.right);
+                nextCount++;
+            }
 
-	    if (currentCount == 0) {
-		currentCount = nextCount;
-		nextCount = 0;
-		System.out.println();
-	    }
-	}
+            if (currentCount == 0) {
+                currentCount = nextCount;
+                nextCount = 0;
+                System.out.println();
+            }
+        }
     }
 
     public static void main(String[] args) {
-	int[] a = {5, 4, 9, 5, 7, 2, 2};
-	TernarySearchTree tst =new TernarySearchTree();
+        int[] a = {5, 4, 9, 5, 7, 2, 2};
+        TernarySearchTree tst =new TernarySearchTree();
 
-	for (int i : a) {
-	    tst.insert(i);
-	}
+        for (int i : a) {
+            tst.insert(i);
+        }
 
-	tst.printLevelOrder();
-	System.out.println();
+        tst.printLevelOrder();
+        System.out.println();
 
-	tst.delete(5);
-	tst.printLevelOrder();
-	System.out.println();
+        tst.delete(5);
+        tst.printLevelOrder();
+        System.out.println();
 
-	tst.delete(9);
-	tst.printLevelOrder();
-	System.out.println();
+        tst.delete(9);
+        tst.printLevelOrder();
+        System.out.println();
 
-	tst.delete(4);
-	tst.printLevelOrder();
-	System.out.println();
+        tst.delete(4);
+        tst.printLevelOrder();
+        System.out.println();
 
-	tst.delete(0);
-	tst.printLevelOrder();
+        tst.delete(0);
+        tst.printLevelOrder();
+        System.out.println();
+
+        tst.insert(5);
+        tst.printLevelOrder();
     }
 }
