3df20859661ff23fb17da677036fe43e7d06318a
Edward <blingwang@gmail.com>
2013-12-02 00:22:34 -0800

    Update Candy.java

--- leetcode/Candy.java
+++ leetcode/Candy.java
@@ -25,4 +25,38 @@ public class Candy {
         
         return totalCandies;
     }
+    
+    public int candy2(int[] ratings) {
+        int totalCandies = 0;
+        int descSeqLen = 0;
+        int preCandyCount = 1;
+        int maxCandyCountInSeq = 1;
+        
+        totalCandies++;
+        
+        for (int i = 1; i < ratings.length; i++) {
+            if (ratings[i] < ratings[i-1]) {
+                descSeqLen++;
+                
+                if (maxCandyCountInSeq == descSeqLen) {
+                    descSeqLen++;
+                }
+                
+                totalCandies += descSeqLen;
+                preCandyCount = 1;
+            } else {
+                if (ratings[i] > ratings[i-1]) {
+                    preCandyCount++;
+                } else {
+                    preCandyCount = 1;
+                }
+                
+                totalCandies += preCandyCount;
+                descSeqLen = 0;
+                maxCandyCountInSeq = preCandyCount;
+            }
+        }
+        
+        return totalCandies;
+    }
 }
