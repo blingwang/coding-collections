57e4266e07377f87650b93e7e3f520755bb14c0d
Edward <blingwang@gmail.com>
2013-12-04 12:57:29 -0600

    Update SearchInsertionPosition.java

--- leetcode/SearchInsertionPosition.java
+++ leetcode/SearchInsertionPosition.java
@@ -1,5 +1,19 @@
 public class SearchInsertionPosition {
     public int searchInsert(int[] A, int target) {
+        int left = 0;
+        int right = A.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (A[mid] < target)        left = mid + 1;
+            else if (A[mid] > target)   right = mid - 1;
+            else                        return mid;
+        }
+        
+        return left; // left is the first greater or equal position
+    }
+    
+    public int searchInsert2(int[] A, int target) {
         int left = 0; 
         int right = A.length - 1;
         int bestSoFar = -1;
