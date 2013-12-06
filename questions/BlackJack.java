3b5fcce3916a027abe12a606c32ae34430f4a761
Edward <blingwang@gmail.com>
2013-12-06 15:17:38 -0600

    Create BlackJack.java

---
+++ questions/BlackJack.java
@@ -0,0 +1,66 @@
+public class BlackJack {
+    private int EvaluateHand(Hand hand)
+    {
+        int score = 0;
+        foreach (Card currentCard in hand)
+        {
+            switch (currentCard.Value)
+            {
+                case Value.Two:
+                    score += 2;
+                    break; 
+                case Value.Three:
+                    score += 3;
+                    break;
+                case Value.Four:
+                    score += 4;
+                    break;
+                case Value.Five:
+                    score += 5;
+                    break;
+                case Value.Six:
+                    score += 6;
+                    break;
+                case Value.Seven:
+                    score += 7;
+                    break;
+                case Value.Eight:
+                    score += 8;
+                    break;
+                case Value.Nine:
+                    score += 9;
+                    break;
+                case Value.Ten:
+                case Value.Jack:
+                case Value.Queen:
+                case Value.King:
+                    score += 10;
+                    break;
+                case Value.Ace:
+                    score += 11;
+                    break; 
+
+            }
+
+        }
+
+        // after evaluating with 11 for each ace, if score has busted, then change each ace value from 11 to 1
+        if (score > 21)
+        {   // if our evaluated score is over 21, subtract 10 foreach ace in the hand. 
+            foreach (Card currentAceCheckCard in hand)
+            {
+                if (score <= 21)
+                {   // if we've subtracted enough until we're not busted, then break and return value
+                    break;
+                }
+                
+                if (currentAceCheckCard.Value == Value.Ace)
+                {
+                    score -= 10;
+                }
+            }
+        }
+        
+        return score; 
+    } 
+}
