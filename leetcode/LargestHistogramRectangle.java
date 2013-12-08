7c242fcc01d1380ae284dbfbe9a6deb13fc9952e
Edward <blingwang@gmail.com>
2013-12-07 19:15:21 -0600

    Update LargestHistogramRectangle.java

--- leetcode/LargestHistogramRectangle.java
+++ leetcode/LargestHistogramRectangle.java
@@ -17,28 +17,32 @@ public class LargestHistogramRectangle {
         return area;
     }
 
-    public int largestRectangleAreaReadableCode(int[] height) {
-        int maxArea = 0;     
-        // stack to store indice with unused height so far
-        // (lower index + 1) on stack is last index with higher or equal height
-        java.util.ArrayDeque<Integer> stack = new java.util.ArrayDeque<Integer>();
-
+    public int largestRectangleAreaReadable(int[] height) {
+        int maxArea = 0;
+        // a stack of unused heights in non-descending order
+        ArrayDeque<Integer> stack = new ArrayDeque<Integer>();
+        
         for (int i = 0; i < height.length; i++) {
             while (!stack.isEmpty() && height[stack.peek()] > height[i]) {
+                // for each height on top of stack, 
+                // left bound is stack.peek(), right bound is i
                 int top = stack.pop();
                 int width = stack.isEmpty() ? i : i - stack.peek() - 1;
-                maxArea = Math.max(maxArea, height[top] * width);
+                int area = height[top] * width;
+                maxArea = Math.max(maxArea, area);
             }
+            
             stack.push(i);
         }
-
+        
         while (!stack.isEmpty()) {
             int top = stack.pop();
             int width = stack.isEmpty() ? height.length : 
                         height.length - stack.peek() - 1;
-            maxArea = Math.max(maxArea, height[top] * width);
+            int area = height[top] * width;
+            maxArea = Math.max(maxArea, area);
         }
-
+        
         return maxArea;
     }
 
