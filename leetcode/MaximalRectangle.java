06a4a96bc401d6d107d0020b07b732c05fd75425
Edward <blingwang@gmail.com>
2013-12-07 19:40:06 -0600

    Update MaximalRectangle.java

--- leetcode/MaximalRectangle.java
+++ leetcode/MaximalRectangle.java
@@ -4,37 +4,38 @@ public class MaximalRectangle {
         int m = matrix.length;
         if (m == 0) return 0;
         int n = matrix[0].length;
+        if (n == 0) return 0;
         
-        // process columns
-        int[][] heightMatrix = new int[m][n];
+        // create height matrix
+        int[][] heights = new int[m][n];
         for (int j = 0; j < n; j++) {
-            heightMatrix[0][j] = matrix[0][j] - '0';
-            for ( int i = 1; i < m; i++) {
-                heightMatrix[i][j] = matrix[i][j] == '0' ? 0 : heightMatrix[i-1][j]+1; 
+            heights[0][j] = matrix[0][j] - '0';
+            for (int i = 1; i < m; i++) {
+                heights[i][j] = matrix[i][j] == '0' ? 0 : heights[i-1][j] + 1;
             }
         }
         
-        // compute largest rectangle area for each row, update max rect 
-        int maxRect = 0;
-        for ( int i = 0; i < m; i++) {
-            maxRect = Math.max(maxRect, largestRectangleArea(heightMatrix[i]));
+        // compute largest reactangle in histogram
+        int maxArea = 0;
+        for (int i = 0; i < m; i++) {
+            maxArea = Math.max(maxArea, largestRectangleArea(heights[i]));
         }
         
-        return maxRect;
+        return maxArea;
     }
-
+    
     private int largestRectangleArea(int[] height) {
-        int maxArea = 0;     
-        // stack to store indice with unused height so far
-        // (lower index + 1) on stack is last index with higher or equal height
+        int maxArea = 0;
         ArrayDeque<Integer> stack = new ArrayDeque<Integer>();
         
         for (int i = 0; i < height.length; i++) {
             while (!stack.isEmpty() && height[stack.peek()] > height[i]) {
                 int top = stack.pop();
                 int width = stack.isEmpty() ? i : i - stack.peek() - 1;
-                maxArea = Math.max(maxArea, height[top] * width);
+                int area = height[top] * width;
+                maxArea = Math.max(maxArea, area);
             }
+            
             stack.push(i);
         }
         
@@ -42,7 +43,8 @@ public class MaximalRectangle {
             int top = stack.pop();
             int width = stack.isEmpty() ? height.length : 
                         height.length - stack.peek() - 1;
-            maxArea = Math.max(maxArea, height[top] * width);
+            int area = height[top] * width;
+            maxArea = Math.max(maxArea, area);
         }
         
         return maxArea;
