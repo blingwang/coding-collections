d809af42fb02c72ed20845b5e6557a4283c0b8b0
Edward <blingwang@gmail.com>
2013-12-08 01:05:27 -0600

    Update LongestConsecutiveSequence.java

--- leetcode/LongestConsecutiveSequence.java
+++ leetcode/LongestConsecutiveSequence.java
@@ -3,24 +3,28 @@ public class LongestConsecutiveSequence {
     public int longestConsecutive(int[] num) {
         int maxLength = 0;
         Set<Integer> numSet = new HashSet<Integer>();
-        for (int i = 0; i < num.length; i++) {
-            numSet.add(num[i]);
+        
+        for (int curNum : num) {
+            numSet.add(curNum);
         }
         
         for (int curNum : num) {
-            maxLength = Math.max(maxLength, getLength(curNum, numSet));
+            maxLength = Math.max(maxLength, getLength(numSet, curNum));
         }
+        
         return maxLength;
     }
-
-    private int getLength(int number, Set<Integer> numSet) {
+    
+    private int getLength(Set<Integer> numSet, int number) {
         int length = 0;
-        int curNum = number;  
+        
+        int curNum = number;
         while (numSet.contains(curNum)) {
-            numSet.remove(curNum);
+            numSet.remove(curNum); // similar to marked in DFS
             length++;
             curNum--;
         }
+        
         curNum = number + 1;
         while (numSet.contains(curNum)) {
             numSet.remove(curNum);
