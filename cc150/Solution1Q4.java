ea5e5b57f0bea7bab675b60df8e4b906f38136b3
Edward <blingwang@gmail.com>
2013-12-08 16:05:46 -0600

    Update Solution1Q4.java

--- cc150/Solution1Q4.java
+++ cc150/Solution1Q4.java
@@ -1,18 +1,12 @@
 public class Solution1Q4{
     // length is true length of the string array
     public void replaceSpaces(char[] str, int length){
-        int spaceCount = 0;
-        for (int i = 0; i < length; i++) {
-            if (str[i] == ' ') {
-                spaceCount++;
-            }
-        }
-        
-        int newLength = length + spaceCount * 2;
-        assert str.length >= newLength;
+        int newLength = length + countSpaces(str) * 2;
+        assert(str.length >= newLength);
         
         int writeIndex = newLength;
         str[writeIndex--] = '\0';  
+        
         for (int i = length - 1; i >= 0; i--) {
             if (str[i] == ' ') {
                 str[writeIndex--] = '%';
@@ -22,5 +16,15 @@ public class Solution1Q4{
                 str[writeIndex--] = str[i];
             }
         }
+    }
+    
+    private int countSpaces(char[] str) {
+        int spaceCount = 0;
+        for (int i = 0; i < length; i++) {
+            if (str[i] == ' ') {
+                spaceCount++;
+            }
+        }
+        return spaceCount;
     }
 }
