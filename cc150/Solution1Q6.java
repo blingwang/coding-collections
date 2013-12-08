71b91443377258e2762fffa615193f2ea1c5811c
Edward <blingwang@gmail.com>
2013-12-08 17:37:59 -0600

    Update Solution1Q6.java

--- cc150/Solution1Q6.java
+++ cc150/Solution1Q6.java
@@ -1,25 +1,15 @@
 public class Solution1Q6{
-    public void rotate(int[][] matrix, int n){
-        for(int layer = 0; layer < n/2; layer++){
-            int first = layer;
-            int last = n - 1 - layer;
-
-            for(int i = first; i < last; i++){
-                int offset = i - first;
-                //save top
-                int top = matrix[first][i];
-
-                // left -> top
-                matrix[first][i] = matrix[last-offset][first];
-
-                // bottom -> left
-                matrix[last-offset][first] = matrix[last][last-offset];
-
-                // right -> bottom
-                matrix[last][last-offset] = matrix[i][last];
-
-                // top -> right
-                matrix[i][last] = top;
+    public void rotateByLayer(int[][] matrix) {
+        int n = matrix.length;
+        assert(n > 0 && n == matrix[0].length);
+        
+        for (int i = 0; i < n / 2; i++) { // i=layer, skip single core for odd n
+            for (int j =i; j < n - 1 - i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = temp;
             }
         }
     }
