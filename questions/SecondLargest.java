96108e3ed89b9a05c352f07790752e0d5f1e463e
Edward <blingwang@gmail.com>
2013-12-09 11:01:37 -0600

    Update SecondLargest.java

--- questions/SecondLargest.java
+++ questions/SecondLargest.java
@@ -1,9 +1,15 @@
 public class SecondLargest {
-    public static int secondHighest(int[] nums) {
+    /* This is O(N) in one pass. If you want to accept ties, 
+     * then change to if (num >= high1), but as it is, 
+     * it will return Integer.MIN_VALUE if there aren't 
+     * at least 2 elements in the array. It will also return 
+     * Integer.MIN_VALUE if the array contains only 
+     * the same number if using (num >= high1).*/
+    public static int secondHighest(int... nums) {
         int high1 = Integer.MIN_VALUE;
         int high2 = Integer.MIN_VALUE;
         for (int num : nums) {
-            if (num >= high1) {
+            if (num > high1) {
                 high2 = high1;
                 high1 = num;
             } else if (num > high2) {
