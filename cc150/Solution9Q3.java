7b24396b234cd285db68b1c843b4b78304e43929
Edward <blingwang@gmail.com>
2013-12-10 02:09:07 -0600

    Update Solution9Q3.java

--- cc150/Solution9Q3.java
+++ cc150/Solution9Q3.java
@@ -1,15 +1,16 @@
 class Solution9Q3 {
     public int findMagicIndex(int[] a) {
-        return findMagicIndex(a, 0, a.length - 1);
-    }
-    
-    private int findMagicIndex(int[] a, int lo, int hi) {
-        if (lo > hi) return -1;
+        int lo = 0; 
+        int hi = a.length - 1;
         
-        int mid = lo + (hi - lo) / 2;
-        if (a[mid] == mid) return mid;
-        else if (a[mid] < mid) return findMagicIndex(a, mid+1, hi);
-        else return findMagicIndex(a, lo, mid-1);
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (a[mid] == mid) return mid;
+            else if (a[mid] < mid) lo = mid+1;
+            else hi = mid-1;
+        }
+        
+        return -1;
     }
     
     public int findMagicIndexDup(int[] a) {
