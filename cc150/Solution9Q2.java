75cf7ef285112ac942f0b2debc5b77d39867e4ee
Edward <blingwang@gmail.com>
2013-12-10 01:09:23 -0600

    Update Solution9Q2.java

--- cc150/Solution9Q2.java
+++ cc150/Solution9Q2.java
@@ -45,29 +45,31 @@ class Solution9Q2 {
     private boolean getPath(int x, int y, ArrayList<Point> path, 
             HashMap<Point, Boolean> cache) {
         Point p = new Point(x, y);
-        if (cache.containsKey(p)) { // already visited this cell
+        
+        if (cache.containsKey(p)) {
             return cache.get(p);
         }
 
         if(x == 0 && y ==0) {
-            return true; // found a path
+            return true;
         }
 
         boolean success = false;
 
-        if (x >= 1 && isFree(x - 1, y)) { // Try right
-            success = getPath(x - 1, y, path, cache); // Free! Go right
+        if (x >= 1 && isFree(x - 1, y)) {
+            success = getPath(x - 1, y, path, cache);
         }
 
-        if (!success && y >= 1 && isFree(x, y - 1)) { // Try down
-            success = getPath(x, y - 1, path, cache); // Free! Go down
+        if (!success && y >= 1 && isFree(x, y - 1)) {
+            success = getPath(x, y - 1, path, cache);
         }
 
         if (success) {
-            path.add(p); // Right way! add to path
+            path.add(p); 
         }
 
-        cache.put(p, success); // cache result
+        cache.put(p, success);
+        
         return success;
     }
 
