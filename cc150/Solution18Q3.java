cc6ca6e548d74ed65402ca39fb8f34113d63a19a
Edward <blingwang@gmail.com>
2013-12-11 10:03:44 -0800

    Update Solution18Q3.java

--- cc150/Solution18Q3.java
+++ cc150/Solution18Q3.java
@@ -6,15 +6,13 @@ class Solution18Q2 {
     public int[] reservoirSampling1(int[] original, int m) {
         int[] subset = new int[m];
         
-        // fill in subset array with first part of original array
         for (int i = 0; i < m; i++) {
             subset[i] = original[i];
         }
 
-        // shuffle rest of original array (insert into previous shuffled)
-        for (int i = m; i < original.length; i++) {
+        for (int i = m; i < original.length; i++) {// shuffle rest
             int k = rand(0, i); 
-            if (k < m) { // swap if in m range, always keep original array
+            if (k < m) { // insert if in m range
                 subset[k] = original[i];
             }
         }
