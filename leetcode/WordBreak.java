5d60c113d9740d01041356a7c0f8bc4ea489a3da
Edward <blingwang@gmail.com>
2013-12-12 16:08:34 -0800

    Update WordBreak.java

--- leetcode/WordBreak.java
+++ leetcode/WordBreak.java
@@ -12,4 +12,28 @@ public class WordBreak {
         
         return false;
     }
+    
+    // memorization
+    public boolean wordBreakWithCache(String s, Set<String> dict) {
+        Map<String, Boolean> cache = new HashMap<String, Boolean>();
+        return wordBreak(s, dict, cache);
+    }
+    public boolean wordBreak(String s, Set<String> dict, 
+                             Map<String, Boolean> cache) {
+        if (cache.containsKey(s)) return cache.get(s);
+        
+        if (dict.contains(s)) return true;
+        
+        for (int i = 1; i < s.length(); i++) {
+            String prefix = s.substring(0, i);
+            if (dict.contains(prefix)) {
+                String suffix = s.substring(i);
+                if (wordBreak(suffix, dict, cache)) return true;
+            }
+        }
+        
+        cache.put(s, false);
+        
+        return false;
+    }
 }
