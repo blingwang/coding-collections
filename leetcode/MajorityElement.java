a6454e0c3c3892238e4ef54af679b0c26f4ac11e
Edward <blingwang@gmail.com>
2015-10-25 14:51:05 -0700

    Update MajorityElement.java

--- leetcode/MajorityElement.java
+++ leetcode/MajorityElement.java
@@ -1,5 +1,33 @@
 public class Solution {
+    /* Voting algorithm */
     public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int candidate = nums[0];
+        int counter = 0;
+        
+        for (int num : nums) {
+            if (counter == 0) {
+                candidate = num;
+            }
+            
+            if (num == candidate) {
+                counter++;
+            } else {
+                counter--;
+            }
+        }
+        
+        counter = 0;
+        for (int num : nums) {
+            if (num == candidate) counter++;
+        }
+        
+        if (counter > n / 2) return candidate;
+        
+        return -1;
+    }
+    
+    public int majorityElementLinearSpace(int[] nums) {
         Map<Integer, Integer> count = new HashMap<Integer, Integer>();
         
         int n = nums.length;
