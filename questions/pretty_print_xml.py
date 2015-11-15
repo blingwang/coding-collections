7eb312a4deb6918a658f641f42fa3056fe31880d
Edward <blingwang@gmail.com>
2015-11-14 17:04:39 -0800

    Update pretty_print_xml.py

--- questions/pretty_print_xml.py
+++ questions/pretty_print_xml.py
@@ -22,3 +22,11 @@ ElementTree.dump(root)
 # element: tag, attributes, text, tail, list of children nodes.
 # text: text before list of children nodes
 # tail: text immediately after a node
+
+# SAX: Is a stream-based processor. 
+# You only have a tiny part in memory at any time and you "sniff" the XML stream as it passes. 
+# It uses almost no memory, but you can't do "DOM" stuff, like use xpath or traverse trees.
+
+# DOM: You load the whole thing into memory - it's a massive memory hog. 
+# You can blow memory with even medium sized documents. 
+# But you can use xpath and traverse the tree etc.
