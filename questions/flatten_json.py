b9a6a7434e7216e36d58f92aa699a465bd923e4b
Edward <blingwang@gmail.com>
2015-11-14 17:44:34 -0800

    Create flatten_json.py

---
+++ questions/flatten_json.py
@@ -0,0 +1,17 @@
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + '_')
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + '_')
+                i += 1
+        else:
+            out[str(name[:-1])] = str(x)
+
+    flatten(y)
+    return out
