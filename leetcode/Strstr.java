f9da6738608d54e45374769bc120e14230ccd72c
Edward <blingwang@gmail.com>
2015-11-15 18:57:32 -0800

    Update Strstr.java

--- leetcode/Strstr.java
+++ leetcode/Strstr.java
@@ -1,4 +1,20 @@
 public class Strstr {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        
+        if (m == 0) return 0;
+        
+        for (int i = 0; i <= n-m; i++) {
+            for (int j = 0; j < m; j++) {
+                if (needle.charAt(j) != haystack.charAt(i + j)) break;
+                if (j == m-1) return i;
+            }
+        }  
+        
+        return -1;
+    }
+    
     public String strStr(String haystack, String needle) {
         int n = haystack.length();
         int m = needle.length();
