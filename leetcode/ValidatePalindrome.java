9e0a68eabcec987cc1b455c7279fd4cf4a9e5d14
Edward <blingwang@gmail.com>
2016-04-25 01:06:04 -0700

    Update ValidatePalindrome.java

--- leetcode/ValidatePalindrome.java
+++ leetcode/ValidatePalindrome.java
@@ -16,7 +16,35 @@ public class ValidatePalindrome {
         
         return true;
     }
-
+    
+    public boolean isPalindromeWithNonWordChars(String s) {
+        if (s.length() == 0) return true;
+        
+        int start = 0;
+        int end = s.length() - 1;
+        
+        while (start < end) {
+            while (start < s.length() && !isWordChar(s.charAt(start))) {
+                start++;
+            }
+            
+            if (start == s.length()) return true;
+            
+            while (end > start && !isWordChar(s.charAt(end))) {
+                end--;
+            }
+            
+            if (start == end) return true;
+            
+            if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) return false;
+            
+            start++;
+            end--;
+        }
+        
+        return true;
+    }
+    
     private boolean isWordChar(char c) {
         return (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z');
     }
