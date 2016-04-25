f9d3ba219726b0d383699cdc55ad6753ba8a11ef
Edward <blingwang@gmail.com>
2016-04-25 00:00:28 -0700

    Update ValidBST.java

--- leetcode/ValidBST.java
+++ leetcode/ValidBST.java
@@ -36,6 +36,30 @@ public class ValidBST {
         
         return true;
     }
+    
+    // inorder 3
+    // private TreeNode pre;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        
+        if (pre != null && pre.val >= root.val) {
+            return false;
+        }
+        
+        pre = root;
+        
+        if (!isValidBST(root.right)) {
+            return false;
+        }
+        
+        return true;
+    }
 
     public class TreeNode {
         int val;
