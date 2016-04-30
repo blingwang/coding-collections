ed98956f52b95c3462566bf7ff9a7d0bcca16e1a
Edward <blingwang@gmail.com>
2016-04-30 10:29:48 -0700

    Create ContainsDuplicate2.java

---
+++ leetcode/ContainsDuplicate2.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) set.remove(nums[i-k-1]);
+            if (!set.add(nums[i])) return true;
+        }
+        return false;
+    }
+}
