21d42cb447b1cbd7f47ff305583f698799f71099
Edward <blingwang@gmail.com>
2016-04-30 23:36:20 -0700

    Create ContainsDuplicate3.java

---
+++ leetcode/ContainsDuplicate3.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (nums.length < 2 || t < 0 || k < 1) return false;
+        
+        Map<Integer, Integer> buckets = new HashMap<>();
+        int width = t + 1;
+        for (int i = 0; i < nums.length; i++) {
+            int id = getBucketId(nums[i], width);
+            
+            if (buckets.containsKey(id)) return true;
+            if (buckets.containsKey(id-1) && nums[i] - buckets.get(id-1) < width) return true;
+            if (buckets.containsKey(id+1) && buckets.get(id+1) - nums[i] < width) return true;
+                
+            buckets.put(id, nums[i]);
+            
+            if (i >= k) buckets.remove(getBucketId(nums[i-k], width));
+        }
+        
+        return false;
+    }
+    
+    private int getBucketId(int i, int w) {
+        return i < 0 ? ((i+1)/w - 1) : i/w;
+    }
+}
