54edebb6c07a9bf06532a9409abb66b17db20532
Jianguo Wang <blingwang@gmail.com>
2018-02-12 18:56:04 -0800

    Update reverse-string.js

--- leetcode/reverse-string.js
+++ leetcode/reverse-string.js
@@ -3,5 +3,8 @@
  * @return {string}
  */
 var reverseString = function(s) {
+    // Using the spread operator to split a string into characters 
+    // has the advantage of handling non-BMP Unicode characters correctly 
+    // (which comprise two “JavaScript characters”).
     return [...s].reverse().join('');
 };
