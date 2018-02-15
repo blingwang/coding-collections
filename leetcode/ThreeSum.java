3f054c009ab1fcf94e5e58d91d30fcefb88cba42
Jianguo Wang <blingwang@gmail.com>
2018-02-15 14:31:41 -0800

    Update ThreeSum.java

--- leetcode/ThreeSum.java
+++ leetcode/ThreeSum.java
@@ -2,39 +2,39 @@ import java.util.*;
 
 public class ThreeSum {
     /* Solution 1 using two pointers*/
-    public ArrayList<ArrayList<Integer>> threeSum(int[] num) {
-        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
-        Arrays.sort(num);
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
         
-        for (int i = 0; i < num.length - 2; i++) {
-            if ( i > 0 && num[i] == num[i-1]) continue;
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
             
-            // Do two sum
-            int start = i + 1; 
-            int end = num.length - 1;
-            while (start < end) {// possible optimization: num[i] <= 0 && num[end] >= 0
-                int sum = num[i] + num[start] + num[end];
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                if (start > i + 1 && nums[start] == nums[start-1]) {
+                    start++;
+                    continue;
+                }
+                
+                if (end < nums.length - 1 && nums[end] == nums[end+1]) {
+                    end--;
+                    continue;
+                }
+                
+                int sum = nums[i] + nums[start] + nums[end];
                 if (sum < 0) {
                     start++;
                 } else if (sum > 0) {
                     end--;
                 } else {
-                    int curStart = num[start];
-                    int curEnd = num[end];
-                    ArrayList<Integer> triplet = new ArrayList<Integer>(3);
-                    
-                    triplet.add(num[i]);
-                    triplet.add(curStart);
-                    triplet.add(curEnd);
+                    List<Integer> triplet = new ArrayList<>();
+                    triplet.add(nums[i]);
+                    triplet.add(nums[start]);
+                    triplet.add(nums[end]);
                     result.add(triplet);
-                    
-                    while(start < end &&  num[start] == curStart) {
-                        start++;
-                    }
-                    
-                    while(end > start && num[end] == curEnd) {
-                        end--;
-                    }
+                    start++;
+                    end--;
                 }
             }
         }
