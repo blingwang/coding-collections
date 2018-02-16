4429ac4e918a519cf664198b51d7cd59b86071da
Jianguo Wang <blingwang@gmail.com>
2018-02-15 23:03:00 -0800

    Update Prime.java

--- white-board/Prime.java
+++ white-board/Prime.java
@@ -38,6 +38,20 @@ class Prime {
     
     	return true;
     }
+    
+    // optimized
+    private static boolean isPrime(int n) {
+        if (n % 2 == 0) return n == 2;
+        if (n % 3 == 0) return n == 3;
+        int step = 4, m = (int)Math.sqrt(n) + 1;
+        for(int i = 5; i < m; step = 6-step, i += step) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     // naive method, for more advanced algorithms, see:
     // http://en.wikipedia.org/wiki/Integer_factorization
