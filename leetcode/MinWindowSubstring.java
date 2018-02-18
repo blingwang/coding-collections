9abbb036c82404cee35cf94abf71e5a73a560de8
Jianguo Wang <blingwang@gmail.com>
2018-02-17 17:37:40 -0800

    Update MinWindowSubstring.java

--- leetcode/MinWindowSubstring.java
+++ leetcode/MinWindowSubstring.java
@@ -1,55 +1,40 @@
-public class MinWindowSubstring {
-    public String minWindow(String S, String T) {
-        if (S.length() == 0 || T.length() == 0) return "";
-        if (S.length() < T.length()) return "";
+class Solution {
+//     1. Use two pointers: start and end to represent a window.
+//     2. Move end to find a valid window.
+//     3. When a valid window is found, move start to find a smaller window.
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
         
-        int minWindowLength = Integer.MAX_VALUE;
-        int minWindowBegin = -1;
-        int minWindowEnd = -1;
-        int[] needToFind = new int[256];
-        int[] hasFound = new int[256];
-        int numMatched = 0;
-        
-        for (int i = 0; i < T.length(); i++) {
-            needToFind[T.charAt(i)]++;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < t.length(); i++) {
+            char c = t.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         
-        // find a window, then slide window when window begin char is found again
-        for (int begin = 0, end = 0; end < S.length(); end++) {
-            char c = S.charAt(end);
-            if (needToFind[c] == 0) continue;
-            if (needToFind[c] > hasFound[c]) numMatched++;
-            hasFound[c]++;
-            
-            // if all chars in T have been found, advance begin to minimize window size
-            // for the first window, advance until first non-trivial char is found
-            // if the first window has been found, advance when begin char is found more than needed
-            if (numMatched == T.length()) { 
-                if (minWindowBegin >= 0 && needToFind[S.charAt(begin)] == hasFound[S.charAt(begin)]) continue;
-                
-                while (needToFind[S.charAt(begin)] == 0 || 
-                       needToFind[S.charAt(begin)] < hasFound[S.charAt(begin)]) {
-                    if (needToFind[S.charAt(begin)] < hasFound[S.charAt(begin)]) {
-                        hasFound[S.charAt(begin)]--;
-                    }
-                    begin++;
-                }
-                
-                // update window after begin advanced
-                int windowLength = end - begin + 1;
-                if (windowLength < minWindowLength) {
-                    minWindowLength = windowLength;
-                    minWindowBegin = begin;
-                    minWindowEnd = end;
-                }
+        int minStart = -1;
+        int minLength = Integer.MAX_VALUE;
+        int numCharsFound = 0;
+        for (int start = 0, end = 0; end < s.length(); end++) {
+            char charEnd = s.charAt(end);
+            if (map.containsKey(charEnd)) {
+                if (map.get(charEnd) > 0) numCharsFound++;
+                map.put(charEnd, map.get(charEnd) - 1);
             }
             
+            while (numCharsFound == t.length()) {
+                if (end - start + 1 < minLength) {
+                    minStart = start;
+                    minLength = end - start + 1;
+                }
+                
+                char charStart = s.charAt(start++);
+                if (map.containsKey(charStart)) {
+                    if (map.get(charStart) == 0) numCharsFound--;
+                    map.put(charStart, map.get(charStart) + 1);
+                } 
+            }
         }
         
-        if (numMatched == T.length()) {
-            return S.substring(minWindowBegin, minWindowEnd + 1);
-        }
-        
-        return "";
+        return minStart == -1 ? "" : s.substring(minStart, minStart + minLength);
     }
 }
