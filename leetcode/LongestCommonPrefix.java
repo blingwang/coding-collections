e42c3a3069e43432ebce8a50c3e02c5e8410ec9f
Jianguo Wang <blingwang@gmail.com>
2018-02-22 10:29:54 -0800

    Update LongestCommonPrefix.java

--- leetcode/LongestCommonPrefix.java
+++ leetcode/LongestCommonPrefix.java
@@ -2,17 +2,17 @@ public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
         if (strs.length == 0) return "";
         
-        for (int i = 0; i < strs[0].length(); i++) {
-            char c = strs[0].charAt(i);
-            
+        String first = strs[0];
+        for (int i = 0; i < first.length(); i++) {
+            char c = first.charAt(i);
             for (int j = 1; j < strs.length; j++) {
-                if (i >= strs[j].length() || c != strs[j].charAt(i)) {
-                   return strs[0].substring(0, i); 
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    return first.substring(0, i);
                 }
             }
         }
         
-        return strs[0];
+        return first;
     }
     
     // sort and compare first and last
