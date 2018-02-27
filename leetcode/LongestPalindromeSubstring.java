1a19d5553e291eb822d0098732fdc803811075f0
Jianguo Wang <blingwang@gmail.com>
2018-02-26 20:13:34 -0800

    Update LongestPalindromeSubstring.java

--- leetcode/LongestPalindromeSubstring.java
+++ leetcode/LongestPalindromeSubstring.java
@@ -1,25 +1,28 @@
 public class LongestPalindromeSubstring {
     public String longestPalindrome(String s) {
-        if (s.length() == 0) return "";
+        if (s.isEmpty()) return "";
         
-        String longest = s.substring(0, 1); // single char string
+        String longest = s.substring(0, 1);
         for (int i = 0; i < s.length() - 1; i++) {
-            String p1 = expandAroundCenter(s, i, i);
-            if (p1.length() > longest.length()) longest = p1;
-            String p2 = expandAroundCenter(s, i, i+1);
-            if (p2.length() > longest.length()) longest = p2;
+            String p1 = extendPalindrome(s, i, i);
+            String p2 = extendPalindrome(s, i, i + 1);
+            if (p1.length() > longest.length()) {
+                longest = p1;
+            }
+            if (p2.length() > longest.length()) {
+                longest = p2;
+            }
         }
         
         return longest;
     }
-
-    private String expandAroundCenter(String s, int center1, int center2) {
-        int left = center1, right = center2;
+    
+    private String extendPalindrome(String s, int left, int right) {
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
         
-        return s.substring(left+1, right);
+        return s.substring(left + 1, right);
     }
 }
