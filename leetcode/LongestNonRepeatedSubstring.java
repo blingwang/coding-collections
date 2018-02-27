ac4882d5f067266e4ecaf4a1499919673621760d
Jianguo Wang <blingwang@gmail.com>
2018-02-26 19:55:58 -0800

    Update LongestNonRepeatedSubstring.java

--- leetcode/LongestNonRepeatedSubstring.java
+++ leetcode/LongestNonRepeatedSubstring.java
@@ -2,23 +2,17 @@ import java.util.*;
 
 public class LongestNonRepeatedSubstring {
     public int lengthOfLongestSubstring(String s) {
-        int maxLength = 0;
-        int start = 0; // non-repeated substring start
-        Map<Character, Integer> map = new HashMap<Character, Integer>();
-        
-        for (int i = 0; i < s.length(); i++) {
-            char cur = s.charAt(i);
-            if (map.containsKey(cur) && map.get(cur) >= start) {
-                int last = map.get(cur);
-                maxLength = Math.max(maxLength, i - start);
-                start = last + 1;
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                j = Math.max(j, map.get(c) + 1);
             }
-            
-            map.put(cur, i);
+            map.put(c, i);
+            max = Math.max(max, i - j + 1);
         }
-        
-        maxLength = Math.max(maxLength, s.length() - start);
-        return maxLength;
+        return max;
     }
     
     public int lengthOfLongestSubstring2(String s) {
