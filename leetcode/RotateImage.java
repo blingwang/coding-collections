4539d16ae84dc0072f93d7f3ddd05d1d2ef59f33
Jianguo Wang <blingwang@gmail.com>
2018-02-26 23:14:24 -0800

    Update RotateImage.java

--- leetcode/RotateImage.java
+++ leetcode/RotateImage.java
@@ -16,6 +16,22 @@ public class RotateImage {
     */
     
     public void rotate(int[][] matrix) {
+        for (int i = 0, j = matrix.length - 1; i < j; i++, j--) {
+            int[] temp = matrix[i];
+            matrix[i] = matrix[j];
+            matrix[j] = temp;
+        }
+        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    
+    public void rotate(int[][] matrix) {
         rotateByTranspose(matrix);
     }
     public void rotateByTranspose(int[][] matrix) {
