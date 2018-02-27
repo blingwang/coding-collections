dd779080d016f7c79d02a79666e02ddce75dd345
Jianguo Wang <blingwang@gmail.com>
2018-02-26 21:36:55 -0800

    Update RotateImage.java

--- leetcode/RotateImage.java
+++ leetcode/RotateImage.java
@@ -1,4 +1,20 @@
 public class RotateImage {
+    /*
+     * clockwise rotate
+     * first reverse up to down, then swap the symmetry 
+     * 1 2 3     7 8 9     7 4 1
+     * 4 5 6  => 4 5 6  => 8 5 2
+     * 7 8 9     1 2 3     9 6 3
+    */
+    
+    /*
+     * anticlockwise rotate
+     * first reverse left to right, then swap the symmetry
+     * 1 2 3     3 2 1     3 6 9
+     * 4 5 6  => 6 5 4  => 2 5 8
+     * 7 8 9     9 8 7     1 4 7
+    */
+    
     public void rotate(int[][] matrix) {
         rotateByTranspose(matrix);
     }
