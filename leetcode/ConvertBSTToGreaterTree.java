f155c23e98995672d7d0008be4ca56326e4f6cb5
Jianguo Wang <blingwang@gmail.com>
2018-02-27 21:44:17 -0800

    Create ConvertBSTToGreaterTree.java

---
+++ leetcode/ConvertBSTToGreaterTree.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int sum = 0;
+    
+    public TreeNode convertBST(TreeNode root) {
+        convert(root);
+        return root;
+    }
+    
+    private void convert(TreeNode root) {
+        if (root == null) return;
+        convert(root.right);
+        root.val += sum;
+        sum = root.val;
+        convert(root.left);
+    } 
+}
