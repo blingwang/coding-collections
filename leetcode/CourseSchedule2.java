f70835cab83870d5f08636e4be4af7cc76220e96
Jianguo Wang <blingwang@gmail.com>
2018-03-07 15:25:41 -0800

    Update CourseSchedule2.java

--- leetcode/CourseSchedule2.java
+++ leetcode/CourseSchedule2.java
@@ -1,21 +1,14 @@
-public class Solution {
-    private boolean marked[];
-    private boolean onStack[];
-    private ArrayDeque<Integer> reversePost;
+class Solution {
+    private boolean[] marked;
+    private boolean[] onStack;
+    private Deque<Integer> reversePost;
     
     public int[] findOrder(int numCourses, int[][] prerequisites) {
+        // return bfs(numCourses, prerequisites);
+        ArrayList<Integer>[] graph = makeGraph(numCourses, prerequisites);
         marked = new boolean[numCourses];
         onStack = new boolean[numCourses];
         reversePost = new ArrayDeque<Integer>();
-        ArrayList<Integer>[] graph = new ArrayList[numCourses];
-        
-        for (int i = 0; i < numCourses; i++) {
-            graph[i] = new ArrayList<Integer>();
-        }
-        
-        for (int[] edge : prerequisites) {
-            graph[edge[1]].add(edge[0]);
-        }
         
         for (int i = 0; i < numCourses; i++) {
             if (!marked[i] && !dfs(graph, i)) {
@@ -31,17 +24,64 @@ public class Solution {
         return order;
     }
     
-    private boolean dfs(ArrayList<Integer>[] graph, int course) {
-        marked[course] = true;
-        onStack[course] = true;
-        
-        for (int c : graph[course]) {
-            if (!marked[c] && !dfs(graph, c)) return false;
-            if (onStack[c]) return false;
+    private ArrayList<Integer>[] makeGraph(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList<Integer>();
+        }
+        for (int[] edge : prerequisites) {
+            graph[edge[1]].add(edge[0]);
+        }
+        return graph;
+    }
+    
+    private boolean dfs(ArrayList<Integer>[] graph, int v) {
+        marked[v] = true;
+        onStack[v] = true;
+        for (int w : graph[v]) {
+            if (!marked[w] && !dfs(graph, w)) {
+                return false;
+            }
+            if (onStack[w]) {
+                return false;
+            }
         }
         
-        onStack[course] = false;
-        reversePost.push(course);
+        onStack[v] = false;
+        reversePost.push(v);
         return true;
     }
+    
+    private int[] bfs(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer>[] graph = makeGraph(numCourses, prerequisites);
+        
+        int[] degrees = new int[numCourses];
+        for (ArrayList<Integer> neighbors : graph) {
+            for (int n : neighbors) {
+                degrees[n]++;
+            }
+        }
+        
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (degrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        
+        int visited = 0;
+        int[] order = new int[numCourses];        
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            order[visited++] = v;
+            for (int w : graph[v]) {
+                degrees[w]--;
+                if (degrees[w] == 0) {
+                    queue.offer(w);
+                }
+            }
+        }
+        
+        return visited == numCourses? order : new int[0];
+    }    
 }
