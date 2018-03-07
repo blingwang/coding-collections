09ad0bb095962fbb847dbd76064d081a3af7767c
Jianguo Wang <blingwang@gmail.com>
2018-03-07 10:53:42 -0800

    Update CourseSchedule.java

--- leetcode/CourseSchedule.java
+++ leetcode/CourseSchedule.java
@@ -1,22 +1,12 @@
 public class Solution {
-    private boolean marked[];
-    private boolean onStack[];
-    
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        marked = new boolean[numCourses];
-        onStack = new boolean[numCourses];
-        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+        // return bfsCycle(numCourses, prerequisites);
+        ArrayList<Integer>[] graph = makeGraph(numCourses, prerequisites);
+        boolean[] marked = new boolean[numCourses];
+        boolean[] onStack = new boolean[numCourses];
         
         for (int i = 0; i < numCourses; i++) {
-            graph[i] = new ArrayList<Integer>();
-        }
-        
-        for (int[] edge : prerequisites) {
-            graph[edge[1]].add(edge[0]);
-        }
-        
-        for (int i = 0; i < numCourses; i++) {
-            if (!marked[i] && !dfs(graph, i)) {
+            if (!marked[i] && !dfsCycle(graph, i, marked, onStack)) {
                 return false;
             }
         }
@@ -24,16 +14,56 @@ public class Solution {
         return true;
     }
     
-    private boolean dfs( ArrayList<Integer>[] graph, int course) {
+    private ArrayList<Integer>[] makeGraph(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            graph[i] = new ArrayList<Integer>();
+        }
+        for (int[] pair : prerequisites) {
+            graph[pair[1]].add(pair[0]);
+        }
+        return graph;
+    }
+    
+    private boolean dfsCycle( ArrayList<Integer>[] graph, int course, boolean[] marked, boolean[] onStack) {
         marked[course] = true;
         onStack[course] = true;
         
         for (int c : graph[course]) {
-            if (!marked[c] && !dfs(graph, c)) return false;
+            if (!marked[c] && !dfsCycle(graph, c, marked, onStack)) return false;
             if (onStack[c]) return false;
         }
         
         onStack[course] = false;
         return true;
+    }
+    
+    public boolean bfsCycle(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer>[] graph = makeGraph(numCourses, prerequisites);
+        int[] degrees = computeIndegree(graph);
+        
+        for (int i = 0; i < numCourses; i++) {
+            int j = 0;
+            while (j < numCourses && degrees[j] != 0) {
+                j++;
+            }
+            if (j == numCourses) return false;
+            degrees[j] = -1;
+            for (int neighbor : graph[j]) {
+                degrees[neighbor]--;
+            }
+        }
+        
+        return true;
+    }
+    
+    private int[] computeIndegree(ArrayList<Integer>[] graph) {
+        int[] degrees = new int[graph.length];
+        for (ArrayList<Integer> neighbors : graph) {
+            for (int n : neighbors) {
+                degrees[n]++;
+            }
+        }
+        return degrees;
     }
 }
