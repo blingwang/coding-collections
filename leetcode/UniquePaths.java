43e90a2558c0e620a5ded3db87e9db3118df32c2
Jianguo Wang <blingwang@gmail.com>
2018-03-15 21:36:31 -0700

    Update UniquePaths.java

--- leetcode/UniquePaths.java
+++ leetcode/UniquePaths.java
@@ -1,4 +1,18 @@
 public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] paths = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    paths[i][j] = 1;
+                } else {
+                    paths[i][j] = paths[i-1][j] + paths[i][j-1];
+                }
+            }
+        }
+        return paths[m-1][n-1];
+    }
+    
     // Solution 1 using recursion + memorization
     private int n;
     private Map<Integer, Integer> cache; 
