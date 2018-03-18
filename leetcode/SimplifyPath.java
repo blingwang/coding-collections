a8caa84809fb4bf799a12c1d49276c8f9e534874
Jianguo Wang <blingwang@gmail.com>
2018-03-17 17:05:54 -0700

    Update SimplifyPath.java

--- leetcode/SimplifyPath.java
+++ leetcode/SimplifyPath.java
@@ -1,12 +1,12 @@
 class Solution {
     public String simplifyPath(String path) {
-        Set<String> skip = new HashSet<>(Arrays.asList("", ".", ".."));
+        Set<String> skipSet = new HashSet<>(Arrays.asList("", ".", ".."));
         Deque<String> deque = new ArrayDeque<>();
         
         for (String dir : path.split("/")) {
             if (dir.equals("..") && !deque.isEmpty()) {
                 deque.removeLast();
-            } else if (!skip.contains(dir)) {
+            } else if (!skipSet.contains(dir)) {
                 deque.addLast(dir);
             }
         }
