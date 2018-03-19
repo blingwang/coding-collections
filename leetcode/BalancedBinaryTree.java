26f902dad2d5943edbd2823cb7ed49aa07411e02
Jianguo Wang <blingwang@gmail.com>
2018-03-18 22:55:07 -0700

    Update BalancedBinaryTree.java

--- leetcode/BalancedBinaryTree.java
+++ leetcode/BalancedBinaryTree.java
@@ -1,45 +1,40 @@
 public class BalancedBinaryTree {
-   public boolean isBalanced(TreeNode root) {
-       return isBalancedBottomUp(root);
-   }
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        int left = depth(root.left);
+        int right = depth(root.right);
+        return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+    
+    private int depth(TreeNode root) {
+        if (root == null) return 0;
+        int left = depth(root.left);
+        int right = depth(root.right);
+        return Math.max(left, right) + 1;
+    }
+    
+    public boolean isBalancedDFS(TreeNode root) {
+        return dfsHeight(root) != -1;
+    }
+    
+    private int dfsHeight (TreeNode root) {
+        if (root == null) return 0;
+        
+        int leftHeight = dfsHeight(root.left);
+        if (leftHeight == -1) return -1;
+        
+        int rightHeight = dfsHeight(root.right);
+        if (rightHeight == -1) return -1;
+        
+        if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+        
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
    
-   public boolean isBalancedBottomUp(TreeNode root) {
-       return checkHeight(root) >= 0;
-   }
-   
-   public int checkHeight(TreeNode root) {
-       if (root == null) return 0;
-       
-       int leftHeight = checkHeight(root.left);
-       if (leftHeight < 0) return -1;
-       
-       int rightHeight = checkHeight(root.right);
-       if (rightHeight < 0) return -1;
-       
-       if (Math.abs(leftHeight - rightHeight) > 1) return -1;
-       
-       return Math.max(leftHeight, rightHeight) + 1;
-   }
-   
-   public boolean isBalancedTopDown(TreeNode root) {
-       if (root == null) return true;
-       
-       int leftHeight = getHeight(root.left);
-       int rightHeight = getHeight(root.right);
-       
-       return Math.abs(leftHeight - rightHeight) <= 1 && 
-              isBalanced(root.left) && isBalanced(root.right);
-   }
-   
-   private int getHeight(TreeNode root) {
-       if (root == null) return 0;
-       return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
-   }
-   
-   private class TreeNode {
+    private class TreeNode {
        int val;
        TreeNode left;
        TreeNode right;
        TreeNode(int x) { val = x; }
-   }
+    }
 }
