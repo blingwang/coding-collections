79996abdec32dcfbc48a755cd2e4d183af664326
Jianguo Wang <blingwang@gmail.com>
2018-03-19 15:02:59 -0700

    Create Calculator.java

---
+++ questions/Calculator.java
@@ -0,0 +1,89 @@
+package com.blingwang.playground;
+
+/* A Java program to evaluate a given expression where tokens are separated
+   by space.
+   Test Cases:
+     "10 + 2 * 6"            ---> 22
+     "100 * 2 + 12"          ---> 212
+     "100 * ( 2 + 12 )"      ---> 1400
+     "100 * ( 2 + 12 ) / 14" ---> 100
+*/
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class EvaluateString {
+    public static int evaluate(String expression) {
+        char[] tokens = expression.toCharArray();
+        Deque<Integer> values = new ArrayDeque<>();
+        Deque<Character> ops = new ArrayDeque<>();
+
+        for (int i = 0; i < tokens.length; i++) {
+            if (tokens[i] == ' ') {
+                continue;
+            }
+            if (tokens[i] >= '0' && tokens[i] <= '9') {
+                int numValue = 0;
+                while (i < tokens.length && Character.isDigit(tokens[i])) {
+                    numValue = numValue * 10 + (tokens[i] - '0');
+                    i++;
+                }
+                values.push(numValue);
+            } else if (tokens[i] == '(') {
+                ops.push(tokens[i]);
+            } else if (tokens[i] == ')') {
+                while (ops.peek() != '(') {
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                }
+                ops.pop();
+            } else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/') {
+                while (!ops.isEmpty() && precedence(ops.peek()) >= precedence(tokens[i])) {
+                    values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+                }
+                ops.push(tokens[i]);
+            }
+        }
+
+        while (!ops.isEmpty()) {
+            values.push(applyOp(ops.pop(), values.pop(), values.pop()));
+        }
+
+        return values.pop();
+    }
+
+    private static int precedence(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+        }
+        return -1;
+    }
+
+    public static int applyOp(char op, int b, int a) {
+        switch (op) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                if (b == 0) {
+                    throw new UnsupportedOperationException("Cannot divide by zero");
+                }
+                return a / b;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(EvaluateString.evaluate("10 + 2 * 6"));
+        System.out.println(EvaluateString.evaluate("100 * 2 + 12"));
+        System.out.println(EvaluateString.evaluate("100 * ( 2 + 12 )"));
+        System.out.println(EvaluateString.evaluate("100 * ( 2 + 12 ) / 14"));
+    }
+}
