a4d88beaafb0c4ce1afeea7ec5911c6f22214ee6
Jianguo Wang <blingwang@gmail.com>
2018-03-20 21:17:54 -0700

    Update LRUCache.java

--- leetcode/LRUCache.java
+++ leetcode/LRUCache.java
@@ -94,3 +94,28 @@ public class LRUCache {
     }
 }
 
+class LRUConcurrentCache {
+    private final Map<K,V> cache;   
+
+    public LRUConcurrentCache(final int maxEntries) {
+        this.cache = new LinkedHashMap<K,V>(maxEntries, 0.75F, true) {
+           private static final long serialVersionUID = -1236481390177598762L;
+           @Override
+           protected boolean removeEldestEntry(Map.Entry<K,V> eldest){            
+              return size() > maxEntries;
+           }
+        };
+    }
+
+    public void put(K key, V value) {
+        synchronized(cache) {
+            cache.put(key, value);
+        }
+    }
+    
+    public V get(K key) {
+        synchronized(cache) {
+            return cache.get(key);
+        }
+    }    
+}
