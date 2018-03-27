9ee722bc5ee1187c8461941465ebb208d01436ce
Jianguo Wang <blingwang@gmail.com>
2018-03-27 00:15:53 -0700

    Update FirstMissingPositive.java

--- leetcode/FirstMissingPositive.java
+++ leetcode/FirstMissingPositive.java
@@ -2,29 +2,28 @@ class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
         
-        for (int i = 0; i < n; i++) {
-            // Check nums[nums[i] - 1] != nums[i] to handle duplicates
-            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
-                swap(nums, i, nums[i] - 1);
-            }
-        } 
-        
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != i + 1) {
-                return i + 1;
+        for(int i = 0; i < n; i++) {
+            while(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums, i, nums[i] - 1);  
             }
         }
         
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+             
         return n + 1;
     }
-    
+
     private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        nums[i] ^= nums[j];
+        nums[j] ^= nums[i];
+        nums[i] ^= nums[j];
     }
     
-    public int firstMissingPositiveUsingSet(int[] nums) {
+    public int firstMissingPositiveHashSet(int[] nums) {
         Set<Integer> positives = new HashSet<>();
         for (int i : nums) {
             if (i > 0 ) {
